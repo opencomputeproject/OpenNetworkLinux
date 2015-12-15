4bc6a97f5d2738833c30fcd19dd5494c2ddc6d6c
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2015-12-15 12:36:16 -0800

    Build output directory is now per-suite.

--- make/config.mk
+++ make/config.mk
@@ -19,7 +19,7 @@ endif
 #
 # The default build directory for all infra:builder compilations is the following.
 #
-export BUILD_DIR_BASE=BUILD
+export BUILD_DIR_BASE=BUILD/$(ONL_DEBIAN_SUITE)
 
 
 # Generate manifest if necessary
@@ -31,6 +31,3 @@ export MODULEMANIFEST := $(shell $(ONL)/tools/scripts/manifest.sh $(ONL) $(BUILD
 ONL_MAKE_FLAGS += --no-print-directory -s
 ONL_MAKE := $(MAKE) $(ONL_MAKE_FLAGS)
 ONL_V_at := @
-
-
-
