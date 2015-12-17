77f879f0ea30067bd7a0c90bae60f95ec731281c
Steven Noble <steven.noble@bigswitch.com>
2015-12-17 02:44:23 +0000

    updated python script and PKG.yml to current standards

--- packages/platforms/accton/x86-64-accton-wedge-16x/platform-config/r0/src/python/x86_64_accton_wedge_16x_r0/__init__.py
+++ packages/platforms/accton/x86-64-accton-wedge-16x/platform-config/r0/src/python/x86_64_accton_wedge_16x_r0/__init__.py
@@ -1,31 +1,16 @@
 from onl.platform.base import *
 from onl.vendor.accton import *
 
-class OpenNetworkPlatformImplementation(OpenNetworkPlatformAccton):
+class OnlPlatform_x86_64_accton_wedge_16x_r0(OpenNetworkPlatformAccton):
 
     def model(self):
-        return "Wedge"
+        return "Wedge-16X"
 
     def platform(self):
         return "x86-64-accton-wedge-16x-r0"
 
-    def _plat_info_dict(self):
-        return {
-            platinfo.LAG_COMPONENT_MAX : 24,
-            platinfo.PORT_COUNT : 16,
-            platinfo.ENHANCED_HASHING : True,
-            platinfo.SYMMETRIC_HASHING : True,
-            }
-
-    def sys_init(self):
-        pass
+    def baseconfig(self):
+        return True
 
     def sys_oid_platform(self):
-        # FIXME
         return ".16.1"
-
-    def baseconfig(self):
-        return os.system(os.path.join(self.platform_basedir(), "boot", "x86-64-accton-wedge-r0-devices.sh")) == 0
-
-if __name__ == "__main__":
-    print OpenNetworkPlatformImplementation()
