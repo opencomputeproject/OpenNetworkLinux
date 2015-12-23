28b743f1d7999ed807528a8037b46d01ccf6cf95
Steven Noble <steven.noble@bigswitch.com>
2015-12-23 17:20:06 +0000

    onlp updates for the as7512_32x and as5812_54x

---
+++ packages/platforms/accton/x86-64-accton-as5812_54x/onlp/builds/src/module/src/platform_lib.h
@@ -0,0 +1,70 @@
+/************************************************************
+ * <bsn.cl fy=2014 v=onl>
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *           Copyright 2015 Accton Technology Corporation.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *
+ * </bsn.cl>
+ ************************************************************
+ *
+ *
+ *
+ ***********************************************************/
+#ifndef __PLATFORM_LIB_H__
+#define __PLATFORM_LIB_H__
+
+#include "x86_64_accton_as5812_54x_log.h"
+
+#define PSU1_ID 1
+#define PSU2_ID 2
+
+#define CHASSIS_FAN_COUNT     5
+#define CHASSIS_THERMAL_COUNT 4 
+#define CHASSIS_LED_COUNT     10 
+
+#define PSU1_AC_PMBUS_PREFIX "/sys/bus/i2c/devices/57-003c/"
+#define PSU2_AC_PMBUS_PREFIX "/sys/bus/i2c/devices/58-003f/"
+
+#define PSU1_AC_HWMON_PREFIX "/sys/bus/i2c/devices/57-0038/"
+#define PSU1_DC_HWMON_PREFIX "/sys/bus/i2c/devices/57-0050/"
+#define PSU2_AC_HWMON_PREFIX "/sys/bus/i2c/devices/58-003b/"
+#define PSU2_DC_HWMON_PREFIX "/sys/bus/i2c/devices/58-0053/"
+
+#define PSU1_AC_HWMON_NODE(node) PSU1_AC_HWMON_PREFIX#node
+#define PSU1_DC_HWMON_NODE(node) PSU1_DC_HWMON_PREFIX#node
+#define PSU2_AC_HWMON_NODE(node) PSU2_AC_HWMON_PREFIX#node
+#define PSU2_DC_HWMON_NODE(node) PSU2_DC_HWMON_PREFIX#node
+
+//#define SFP_HWMON_PREFIX "/sys/bus/i2c/devices/3-0050/"
+//#define SFP_HWMON_NODE(node)     SFP_HWMON_PREFIX#node
+
+#define IDPROM_PATH "/sys/devices/pci0000:00/0000:00:13.0/i2c-1/1-0057/eeprom"
+
+int deviceNodeWriteInt(char *filename, int value, int data_len);
+int deviceNodeReadBinary(char *filename, char *buffer, int buf_size, int data_len);
+int deviceNodeReadString(char *filename, char *buffer, int buf_size, int data_len);
+
+typedef enum psu_type {
+    PSU_TYPE_UNKNOWN,
+    PSU_TYPE_AC_F2B,
+    PSU_TYPE_AC_B2F,
+    PSU_TYPE_DC_48V_F2B,
+    PSU_TYPE_DC_48V_B2F
+} psu_type_t;
+
+psu_type_t get_psu_type(int id, char* modelname, int modelname_len);
+
+#endif  /* __PLATFORM_LIB_H__ */
