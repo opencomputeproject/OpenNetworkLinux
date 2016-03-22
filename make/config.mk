196d308508f05b8a5417f5b9a7f90fbce5d6119d
Carl D. Roth <carl.roth@bigswitch.com>
2016-03-21 17:32:15 -0700

    Support verbose building

--- make/config.mk
+++ make/config.mk
@@ -7,7 +7,7 @@ ifndef ONL
   $(error $$ONL is not defined)
 endif
 
-ifndef ONLPM_PY
+ifndef ONLPM
   ONLPM := $(ONL)/tools/onlpm.py
 endif
 
@@ -25,16 +25,41 @@ endif
 
 export BUILD_DIR_BASE=BUILD/$(ONL_DEBIAN_SUITE)
 
-
 # Generate manifest if necessary
 export MODULEMANIFEST := $(shell $(BUILDER)/tools/mmg.py --dirs $(ONL) $(ONLPM_OPTION_PACKAGEDIRS) --out $(ONL)/make/module-manifest.mk --only-if-missing make)
 
 #
 # Default make options.
 #
-ONL_MAKE_FLAGS += --no-print-directory -s
-ONL_MAKE := $(MAKE) $(ONL_MAKE_FLAGS)
+ifeq ("$(origin V)", "command line")
+VERBOSE := $(V)
+endif
+
+ifneq ($(VERBOSE),1)
+
+# quiet settings
+ONL_V_P := false
 ONL_V_at := @
+ONL_V_GEN = @set -e; echo GEN $@;
+
+else
+
+# verbose settings
+ONL_V_P := :
+
+endif
+
+ifneq ($(VERBOSE),1)
+
+ONL_MAKE_FLAGS += --no-print-directory -s
+
+else
+
+ONL_MAKE_FLAGS += V=1
+
+endif
+
+ONL_MAKE := $(MAKE) $(ONL_MAKE_FLAGS)
 
 #
 # Some build and autogen tools require these settings.
