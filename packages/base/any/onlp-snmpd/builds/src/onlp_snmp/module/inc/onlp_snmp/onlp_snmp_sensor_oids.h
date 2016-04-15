07afc973c01cd062c0c788cf65b6033aadf26c39
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2016-04-15 01:48:48 +0000

    - Use new official OCP ONL subtree. - Implement the ONL Platform information subtrees

--- packages/base/any/onlp-snmpd/builds/src/onlp_snmp/module/inc/onlp_snmp/onlp_snmp_sensor_oids.h
+++ packages/base/any/onlp-snmpd/builds/src/onlp_snmp/module/inc/onlp_snmp/onlp_snmp_sensor_oids.h
@@ -1,21 +1,21 @@
 /************************************************************
  * <bsn.cl fy=2015 v=onl>
- * 
- *           Copyright 2015 Big Switch Networks, Inc.          
- * 
+ *
+ *           Copyright 2015 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  ************************************************************
  *
@@ -27,17 +27,10 @@
 
 /**
  * See:
- *      ONLP-SENSOR-MIBS.txt
+ *      OCP-ONL-SENSOR-MIBS.txt
  */
 
-/**
- * These are all currently defined as part of the Big Switch enterprise tree.
- *
- * In the future we should apply for an ONL Private Enterprise Number and
- * redefine these appropriately.
- */
-
-#define ONLP_SNMP_SENSOR_OID          1,3,6,1,4,1,37538,2,3
+#define ONLP_SNMP_SENSOR_OID          1,3,6,1,4,1,42623,1,2
 
 #define ONLP_SNMP_SENSOR_ENTRY           1
 
