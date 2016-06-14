e731daed092d1a438909c6d4b610eeaabe9ffce0
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2016-06-14 15:58:43 +0000

    Platform mini-cleanup.

--- packages/platforms/accton/x86-64/x86-64-facebook-wedge100/platform-config/r0/src/python/x86_64_facebook_wedge100_r0/__init__.py
+++ packages/platforms/accton/x86-64/x86-64-facebook-wedge100/platform-config/r0/src/python/x86_64_facebook_wedge100_r0/__init__.py
@@ -1,16 +1,8 @@
 from onl.platform.base import *
 from onl.vendor.accton import *
 
-class OnlPlatform_x86_64_facebook_wedge_100_r0(OpenNetworkPlatformAccton):
-
-    def model(self):
-        return "Wedge-100"
-
-    def platform(self):
-        return "x86-64-facebook-wedge100-r0"
-
-    def baseconfig(self):
-        return True
-
-    def sys_oid_platform(self):
-        return ".100.1"
+class OnlPlatform_x86_64_facebook_wedge_100_r0(OnlPlatformAccton,
+                                               OnlPlatformPortConfig_32x100):
+    MODEL="Wedge-100"
+    PLATFORM="x86-64-facebook-wedge100-r0"
+    SYS_OBJECT_ID=".100.1"
