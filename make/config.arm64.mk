8e652d00578ca49b89f709f15c1a6d88dc076243
Shengzhou Liu <Shengzhou.Liu@nxp.com>
2016-07-22 21:47:44 +0800

    Add arm64 support for toolchain in builder8:1.5
    Signed-off-by: Shengzhou Liu <Shengzhou.Liu@nxp.com>

---
+++ make/config.arm64.mk
@@ -0,0 +1,9 @@
+############################################################
+#
+# Open Network Linux
+#
+############################################################
+include $(ONL)/make/config.mk
+export TOOLCHAIN := aarch64-linux-gnu
+export ARCH := arm64
+export UARCH := ARM64
