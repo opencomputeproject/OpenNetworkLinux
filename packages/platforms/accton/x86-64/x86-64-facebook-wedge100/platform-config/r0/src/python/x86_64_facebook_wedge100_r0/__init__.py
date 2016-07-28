7ef753f58d763bb694b47701835f77e3ee2d692b
Steven Noble <steven.noble@bigswitch.com>
2016-07-27 22:02:02 -0700

    updates Facebook Wedge 100 ONIE id

--- packages/platforms/accton/x86-64/x86-64-facebook-wedge100/platform-config/r0/src/python/x86_64_facebook_wedge100_r0/__init__.py
+++ packages/platforms/accton/x86-64/x86-64-facebook-wedge100/platform-config/r0/src/python/x86_64_facebook_wedge100_r0/__init__.py
@@ -1,7 +1,7 @@
 from onl.platform.base import *
 from onl.platform.accton import *
 
-class OnlPlatform_x86_64_facebook_wedge_100_r0(OnlPlatformAccton,
+class OnlPlatform_x86_64_facebook_wedge100_r0(OnlPlatformAccton,
                                                OnlPlatformPortConfig_32x100):
     MODEL="Wedge-100"
     PLATFORM="x86-64-facebook-wedge100-r0"
