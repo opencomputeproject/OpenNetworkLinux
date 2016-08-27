6166b37d01d6b95047f6680300134455d30934ee
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2016-08-27 15:09:15 +0000

    Allow customization and additinal arguments while building the image.
    - Execute custom commands prior to execution of switool.
    - Allow custom arguments when invoking switool.

--- make/swi.mk
+++ make/swi.mk
@@ -19,7 +19,10 @@ endif
 swi: FORCE
 	$(ONL_V_at) rm -rf *.swi* manifest.json
 	$(ONL_V_at) $(ONLPM) $(LINK_OPTIONS)
-	$(ONL_V_at) $(ONL)/tools/switool.py --create --rootfs rootfs-$(ARCH).sqsh --manifest manifest.json $@
+ifdef ONL_PRE_SWITOOL_CMDS
+	$(ONL_V_at) $(ONL_PRE_SWITOOL_CMDS)
+endif
+	$(ONL_V_at) $(ONL)/tools/switool.py --create $(ONL_SWITOOL_EXTRA_ARGS) --rootfs rootfs-$(ARCH).sqsh --manifest manifest.json $@
 	$(ONL_V_at) mv $@ `$(FILENAMER) --type swi --manifest manifest.json $@`
 	$(ONL_V_at) for f in `ls *.swi`; do md5sum $$f > $$f.md5sum; done
 	$(ONL_V_at) rm -rf rootfs-$(ARCH).sqsh
