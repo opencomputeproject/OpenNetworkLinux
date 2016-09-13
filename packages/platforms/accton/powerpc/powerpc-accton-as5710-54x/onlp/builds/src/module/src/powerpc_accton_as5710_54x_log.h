74a4ca31d4e030d05148c3e31a763823339be758
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2016-09-13 10:03:11 -0700

    - Cannonicalize module name (remove revision).

--- packages/platforms/accton/powerpc/powerpc-accton-as5710-54x/onlp/builds/src/module/src/powerpc_accton_as5710_54x_log.h
+++ packages/platforms/accton/powerpc/powerpc-accton-as5710-54x/onlp/builds/src/module/src/powerpc_accton_as5710_54x_log.h
@@ -3,10 +3,10 @@
  * 
  *
  *****************************************************************************/
-#ifndef __POWERPC_ACCTON_AS5710_54X_R0_LOG_H__
-#define __POWERPC_ACCTON_AS5710_54X_R0_LOG_H__
+#ifndef __POWERPC_ACCTON_AS5710_54X_LOG_H__
+#define __POWERPC_ACCTON_AS5710_54X_LOG_H__
 
 #define AIM_LOG_MODULE_NAME powerpc_accton_as5710_54x
 #include <AIM/aim_log.h>
 
-#endif /* __POWERPC_ACCTON_AS5710_54X_R0_LOG_H__ */
+#endif /* __POWERPC_ACCTON_AS5710_54X_LOG_H__ */
