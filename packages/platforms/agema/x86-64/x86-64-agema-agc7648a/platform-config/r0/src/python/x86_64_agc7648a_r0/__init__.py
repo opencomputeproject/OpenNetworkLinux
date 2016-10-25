7efd5ea371bd58d9d1929b4cb8a3793424765130
Sridhar Pitchai <sridhar.pitchai@gmail.com>
2016-10-25 11:48:32 -0700

    Platform x86-64-agema-agc7648     updated to the new ONIE platfrom string

--- packages/platforms/agema/x86-64/x86-64-agema-agc7648a/platform-config/r0/src/python/x86_64_agc7648a_r0/__init__.py
+++ packages/platforms/agema/x86-64/x86-64-agema-agc7648a/platform-config/r0/src/python/x86_64_agc7648a_r0/__init__.py
@@ -1,8 +1,8 @@
 from onl.platform.base import *
-from onl.platform.accton import *
+from onl.platform.agema import *
 
-class OnlPlatform_x86_64_agc7648_r0(OnlPlatformAgema,
+class OnlPlatform_x86_64_agc7648a_r0(OnlPlatformAgema,
                                                OnlPlatformPortConfig_48x10_6x40):
     MODEL="agc7648"
-    PLATFORM="x86-64-agc7648-r0"
+    PLATFORM="x86-64-agc7648a-r0"
     SYS_OBJECT_ID=".7648.1"
