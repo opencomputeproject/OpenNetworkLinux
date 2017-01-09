80afad6a762b7d5fca33990eb93fb94b97d865d3
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2017-01-09 18:34:49 +0000

    - Initial LY9 Support
      Most of this code is originally from Jonathan Tsai's LY9 pull request:
      https://github.com/opencomputeproject/OpenNetworkLinux/pull/102
    
      The LY9 (and other quanta x86 platforms) have all been ported to the 3.16 LTS kernel
      with platform module support. The code from Jonathan's pull request has been manually
      merged into this porting effort where appropriate to provide a new baseline for the
      LY9 development.
    
      The other items originally in the pull request will be reviewed separately.

---
+++ packages/platforms/quanta/x86-64/x86-64-quanta-ly9-rangeley/onlp/builds/src/x86_64_quanta_ly9_rangeley/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_quanta_ly9_rangeley Autogeneration
+#
+###############################################################################
+x86_64_quanta_ly9_rangeley_AUTO_DEFS := module/auto/x86_64_quanta_ly9_rangeley.yml
+x86_64_quanta_ly9_rangeley_AUTO_DIRS := module/inc/x86_64_quanta_ly9_rangeley module/src
+include $(BUILDER)/auto.mk
+
---
+++ packages/platforms/quanta/x86-64/x86-64-quanta-ly9-rangeley/onlp/builds/src/x86_64_quanta_ly9_rangeley/module/src/x86_64_quanta_ly9_rangeley_log.c
@@ -0,0 +1,18 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_quanta_ly9_rangeley/x86_64_quanta_ly9_rangeley_config.h>
+
+#include "x86_64_quanta_ly9_rangeley_log.h"
+/*
+ * x86_64_quanta_ly9_rangeley log struct.
+ */
+AIM_LOG_STRUCT_DEFINE(
+                      X86_64_QUANTA_LY9_RANGELEY_CONFIG_LOG_OPTIONS_DEFAULT,
+                      X86_64_QUANTA_LY9_RANGELEY_CONFIG_LOG_BITS_DEFAULT,
+                      NULL, /* Custom log map */
+                      X86_64_QUANTA_LY9_RANGELEY_CONFIG_LOG_CUSTOM_BITS_DEFAULT
+                     );
+
