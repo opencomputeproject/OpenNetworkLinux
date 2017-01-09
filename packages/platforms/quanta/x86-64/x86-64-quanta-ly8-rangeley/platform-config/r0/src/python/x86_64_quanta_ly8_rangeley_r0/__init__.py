73197b900f9475c135558b3b87ac713c3246fd1d
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2017-01-09 18:39:08 +0000

    These script packages for platform management cannot be used. They will need to be provided by the ONLP platform management implementation.

--- packages/platforms/quanta/x86-64/x86-64-quanta-ly8-rangeley/platform-config/r0/src/python/x86_64_quanta_ly8_rangeley_r0/__init__.py
+++ packages/platforms/quanta/x86-64/x86-64-quanta-ly8-rangeley/platform-config/r0/src/python/x86_64_quanta_ly8_rangeley_r0/__init__.py
@@ -12,25 +12,8 @@ class OnlPlatform_x86_64_quanta_ly8_rangeley_r0(OnlPlatformQuanta,
         self.insmod("quanta_hwmon")
         self.insmod("quanta_switch", params=dict(platform="x86-64-quanta-ly8-rangeley"))
 
-        # fixme
-        try:
-            files = os.listdir("%s/etc/init.d" % self.basedir_onl())
-            for file in files:
-                src = "%s/etc/init.d/%s" % (self.basedir_onl(), file)
-                dst = "/etc/init.d/%s" % file
-                os.system("cp -f %s %s" % (src, dst))
-                os.system("/usr/sbin/update-rc.d %s defaults" % file)
-        except:
-            pass
-
         # make ds1339 as default rtc
         os.system("ln -snf /dev/rtc1 /dev/rtc")
         os.system("hwclock --hctosys")
-
-        # fixme
-        # set system led to green
-        sled = self.basedir_onl('sbin', 'systemled')
-        if os.path.exists(sled):
-            os.system(sled)
 
         return True
