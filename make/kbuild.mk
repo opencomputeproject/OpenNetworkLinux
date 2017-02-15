7196468687180b1cf171e12ccf24c85aa8d166d2
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2017-02-15 18:38:27 +0000

    Include DTS trees.

--- make/kbuild.mk
+++ make/kbuild.mk
@@ -164,7 +164,10 @@ endif
 endif
 
 
-MODSYNCLIST_DEFAULT := .config Module.symvers Makefile include scripts arch/x86/include arch/x86/Makefile arch/powerpc/include arch/powerpc/Makefile arch/powerpc/lib arch/arm/include arch/arm/Makefile arch/arm/lib drivers
+MODSYNCLIST_DEFAULT := .config Module.symvers Makefile include scripts drivers \
+			arch/x86/include arch/x86/Makefile \
+			arch/powerpc/include arch/powerpc/Makefile arch/powerpc/lib arch/powerpc/boot/dts \
+			arch/arm/include arch/arm/Makefile arch/arm/lib
 
 MODSYNCLIST := $(MODSYNCLIST_DEFAULT) $(MODSYNCLIST_EXTRA)
 
