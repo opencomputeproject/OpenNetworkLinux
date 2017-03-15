85e7cecb285460c292453f962911d37e35382cec
MASAN.XU <MASAN.XU@DELTA.COM.TW>
2017-03-15 14:34:47 +0800

    1. add the first editon of agc7648a branch
    Signed-off-by: MASAN.XU <MASAN.XU@DELTA.COM.TW>

---
+++ packages/platforms/delta/x86-64/x86-64-delta-agc7648a/onlp/builds/src/module/inc/x86_64_delta_agc7648a/x86_64_delta_agc7648a.x
@@ -0,0 +1,14 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_delta_agc7648a/x86_64_delta_agc7648a_config.h>
+
+/* <--auto.start.xmacro(ALL).define> */
+/* <auto.end.xmacro(ALL).define> */
+
+/* <--auto.start.xenum(ALL).define> */
+/* <auto.end.xenum(ALL).define> */
+
+
---
+++ packages/platforms/delta/x86-64/x86-64-delta-agc7648a/onlp/builds/src/module/src/x86_64_delta_agc7648a_log.c
@@ -0,0 +1,18 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_delta_agc7648a/x86_64_delta_agc7648a_config.h>
+
+#include "x86_64_delta_agc7648a_log.h"
+/*
+ * x86_64_delta_agc7648a log struct.
+ */
+AIM_LOG_STRUCT_DEFINE(
+                      x86_64_delta_agc7648a_CONFIG_LOG_OPTIONS_DEFAULT,
+                      x86_64_delta_agc7648a_CONFIG_LOG_BITS_DEFAULT,
+                      NULL, /* Custom log map */
+                      x86_64_delta_agc7648a_CONFIG_LOG_CUSTOM_BITS_DEFAULT
+                     );
+
