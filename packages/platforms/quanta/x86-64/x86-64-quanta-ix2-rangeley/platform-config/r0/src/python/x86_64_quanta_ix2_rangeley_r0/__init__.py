109eae5e9609a7dd250657df6dcec2e589272be2
Jonathan Tsai <jonathan.tsai@quantatw.com>
2017-06-27 11:10:38 +0800

    Update Quanta SYS_OBJECT_ID for each platform

--- packages/platforms/quanta/x86-64/x86-64-quanta-ix2-rangeley/platform-config/r0/src/python/x86_64_quanta_ix2_rangeley_r0/__init__.py
+++ packages/platforms/quanta/x86-64/x86-64-quanta-ix2-rangeley/platform-config/r0/src/python/x86_64_quanta_ix2_rangeley_r0/__init__.py
@@ -5,7 +5,15 @@ class OnlPlatform_x86_64_quanta_ix2_rangeley_r0(OnlPlatformQuanta,
                                                 OnlPlatformPortConfig_48x25_8x100):
     PLATFORM='x86-64-quanta-ix2-rangeley-r0'
     MODEL="IX2"
-    SYS_OBJECT_ID=".8.1"
+    """ Define Quanta SYS_OBJECT_ID rule.
+
+    SYS_OBJECT_ID = .xxxx.ABCC
+    "xxxx" define QCT device mark. For example, LB9->1048, LY2->3048
+    "A" define QCT switch series name: LB define 1, LY define 2, IX define 3
+    "B" define QCT switch series number 1: For example, LB9->9, LY2->2
+    "CC" define QCT switch series number 2: For example, LY2->00, LY4R->18(R is 18th english letter)
+    """
+    SYS_OBJECT_ID=".4048.3200"
 
     def baseconfig(self):
         # Expose PSOC that behind PCA9641
--- packages/platforms/quanta/x86-64/x86-64-quanta-ly9-rangeley/platform-config/r0/src/python/x86_64_quanta_ly9_rangeley_r0/__init__.py
+++ packages/platforms/quanta/x86-64/x86-64-quanta-ly9-rangeley/platform-config/r0/src/python/x86_64_quanta_ly9_rangeley_r0/__init__.py
@@ -5,7 +5,15 @@ class OnlPlatform_x86_64_quanta_ly9_rangeley_r0(OnlPlatformQuanta,
                                                 OnlPlatformPortConfig_48x10_6x40):
     PLATFORM='x86-64-quanta-ly9-rangeley-r0'
     MODEL="LY9"
-    SYS_OBJECT_ID=".9.1"
+    """ Define Quanta SYS_OBJECT_ID rule.
+
+    SYS_OBJECT_ID = .xxxx.ABCC
+    "xxxx" define QCT device mark. For example, LB9->1048, LY2->3048
+    "A" define QCT switch series name: LB define 1, LY define 2, IX define 3
+    "B" define QCT switch series number 1: For example, LB9->9, LY2->2
+    "CC" define QCT switch series number 2: For example, LY2->00, LY4R->18(R is 18th english letter)
+    """
+    SYS_OBJECT_ID=".3048.2900"
 
     def baseconfig(self):
         self.insmod("emerson700")
