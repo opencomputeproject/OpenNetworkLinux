da59021e20a62c97353ac318b7b9ec4d97fef589
SphoortiMetri <sphoorti.metri@sjsu.edu>
2017-07-24 17:47:04 -0700

    Add onlpd thermal test cases

---
+++ onlpd-test/Thermal/thermal_tc1.py
@@ -0,0 +1,14 @@
+from libThermal import thermal
+from libThermal import get_thermals
+from time import sleep
+
+Thermalobj = get_thermals()
+count = len(Thermalobj)
+print "The count is ",count
+
+DEBUG = True
+if DEBUG:
+    if count != 0:
+        print "Test case passed"
+    else:
+        print "Test case failed"
