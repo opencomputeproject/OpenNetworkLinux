91fae41cc67cd7d9feb0f2ed75029226bfe81b6b
Brandon Chuang <brandon_chuang@accton.com>
2017-10-16 14:40:44 +0800

    Add new accton platform, AS7816_64X.

---
+++ packages/platforms/accton/x86-64/x86-64-accton-as7816-64x/onlp/builds/src/module/inc/x86_64_accton_as7816_64x/x86_64_accton_as7816_64x_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_accton_as7816_64x Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __X86_64_ACCTON_AS7816_64X_DOX_H__
+#define __X86_64_ACCTON_AS7816_64X_DOX_H__
+
+/**
+ * @defgroup x86_64_accton_as7816_64x x86_64_accton_as7816_64x - x86_64_accton_as7816_64x Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_accton_as7816_64x-x86_64_accton_as7816_64x Public Interface
+ * @defgroup x86_64_accton_as7816_64x-config Compile Time Configuration
+ * @defgroup x86_64_accton_as7816_64x-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __X86_64_ACCTON_AS7816_64X_DOX_H__ */
