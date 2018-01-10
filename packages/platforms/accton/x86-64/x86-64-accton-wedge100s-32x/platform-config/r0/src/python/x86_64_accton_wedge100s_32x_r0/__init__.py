438cf371915863fbd1bb4ba398cf681f21f919f9
Steven Noble <steven.noble@bigswitch.com>
2018-01-10 22:28:32 +0000

    Adding support for Wedge 100S

---
+++ packages/platforms/accton/x86-64/x86-64-accton-wedge100s-32x/platform-config/r0/src/python/x86_64_accton_wedge100s_32x_r0/__init__.py
@@ -0,0 +1,8 @@
+from onl.platform.base import *
+from onl.platform.accton import *
+
+class OnlPlatform_x86_64_accton_wedge100s_32x_r0(OnlPlatformAccton,
+                                               OnlPlatformPortConfig_32x100):
+    MODEL="Wedge-100s-32x"
+    PLATFORM="x86-64-accton-wedge100s-32x-r0"
+    SYS_OBJECT_ID=".100.2"
