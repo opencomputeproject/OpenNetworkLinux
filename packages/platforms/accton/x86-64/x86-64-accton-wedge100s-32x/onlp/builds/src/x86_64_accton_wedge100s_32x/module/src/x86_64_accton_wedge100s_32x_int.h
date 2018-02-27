db5237de9f0c3a530738152b1fa7ba55ace5e20d
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2018-02-27 19:59:52 +0000

    Initial support for the Wedge100s.

---
+++ packages/platforms/accton/x86-64/x86-64-accton-wedge100s-32x/onlp/builds/src/x86_64_accton_wedge100s_32x/module/src/x86_64_accton_wedge100s_32x_int.h
@@ -0,0 +1,12 @@
+/**************************************************************************//**
+ *
+ * x86_64_accton_wedge100s_32x Internal Header
+ *
+ *****************************************************************************/
+#ifndef __X86_64_ACCTON_WEDGE100S_32X_INT_H__
+#define __X86_64_ACCTON_WEDGE100S_32X_INT_H__
+
+#include <x86_64_accton_wedge100s_32x/x86_64_accton_wedge100s_32x_config.h>
+
+
+#endif /* __X86_64_ACCTON_WEDGE100S_32X_INT_H__ */
