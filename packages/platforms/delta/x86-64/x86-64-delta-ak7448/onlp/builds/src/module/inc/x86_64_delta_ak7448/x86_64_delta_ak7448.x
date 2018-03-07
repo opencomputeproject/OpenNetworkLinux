2db62a6ec8a73f8ba003959c26a7f6cd093999f6
johnson.lu <johnson.lu@deltaww.com>
2018-03-07 10:19:10 +0800

    The machine have 48x10GbE SFP28 and 4x100GbE QSFP28 on broadcom BCM56768 Maverick switch ASIC. CPU module is Intel Rangeley C2338 and with SSD 8G.
    Signed-off-by: johnson.lu <johnson.lu@deltaww.com>

---
+++ packages/platforms/delta/x86-64/x86-64-delta-ak7448/onlp/builds/src/module/inc/x86_64_delta_ak7448/x86_64_delta_ak7448.x
@@ -0,0 +1,14 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_delta_ak7448/x86_64_delta_ak7448_config.h>
+
+/* <--auto.start.xmacro(ALL).define> */
+/* <auto.end.xmacro(ALL).define> */
+
+/* <--auto.start.xenum(ALL).define> */
+/* <auto.end.xenum(ALL).define> */
+
+
---
+++ packages/platforms/delta/x86-64/x86-64-delta-ak7448/onlp/builds/src/module/src/x86_64_delta_ak7448_log.c
@@ -0,0 +1,18 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_delta_ak7448/x86_64_delta_ak7448_config.h>
+
+#include "x86_64_delta_ak7448_log.h"
+/*
+ * x86_64_delta_ak7448 log struct.
+ */
+AIM_LOG_STRUCT_DEFINE(
+                      X86_64_DELTA_AK7448_CONFIGLOG_OPTIONS_DEFAULT,
+                      X86_64_DELTA_AK7448_CONFIGLOG_BITS_DEFAULT,
+                      NULL, /* Custom log map */
+                      X86_64_DELTA_AK7448_CONFIGLOG_CUSTOM_BITS_DEFAULT
+                     );
+
