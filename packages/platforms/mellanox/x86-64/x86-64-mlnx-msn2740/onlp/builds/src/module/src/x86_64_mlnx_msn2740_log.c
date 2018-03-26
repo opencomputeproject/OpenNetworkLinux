f4fad36ecaabebeb20554641f5745a48edea8e59
Nataliya Yakuts <nataliyay@mellanox.com>
2018-03-26 15:00:26 +0000

    MSN2740 platform support
    Signed-off-by: Nataliya Yakuts <nataliyay@mellanox.com>
    Reviewed-by: Michael Shych <michaelsh@mellanox.com>

---
+++ packages/platforms/mellanox/x86-64/x86-64-mlnx-msn2740/onlp/builds/src/module/src/x86_64_mlnx_msn2740_log.c
@@ -0,0 +1,17 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_mlnx_msn2740/x86_64_mlnx_msn2740_config.h>
+
+#include "x86_64_mlnx_msn2740_log.h"
+/*
+ * x86_64_mlnx_msn2740 log struct.
+ */
+AIM_LOG_STRUCT_DEFINE(
+                      X86_64_MLNX_MSN2740_CONFIG_LOG_OPTIONS_DEFAULT,
+                      X86_64_MLNX_MSN2740_CONFIG_LOG_BITS_DEFAULT,
+                      NULL, /* Custom log map */
+                      X86_64_MLNX_MSN2740_CONFIG_LOG_CUSTOM_BITS_DEFAULT
+                     );
