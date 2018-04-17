64d353a1ee1b3642fa9b8e072f567b7ae980ffe5
roy_lee <roy_lee@accton.com>
2018-04-17 17:13:00 +0800

    [platform] correct SYS_OBJECT_ID and replace local file write API to one in onlplib. Signed-off-by: roy_lee <roy_lee@accton.com>

--- packages/platforms/accton/x86-64/x86-64-accton-as7326-56x/onlp/builds/src/module/src/platform_lib.c
+++ packages/platforms/accton/x86-64/x86-64-accton-as7326-56x/onlp/builds/src/module/src/platform_lib.c
@@ -5,41 +5,12 @@
 #include "x86_64_accton_as7326_56x_log.h"
 
 
-static int _onlp_file_write(char *filename, char *buffer, int buf_size, int data_len)
-{
-    int fd;
-    int len;
-
-    if ((buffer == NULL) || (buf_size < 0)) {
-        return -1;
-    }
-
-    if ((fd = open(filename, O_WRONLY, S_IWUSR)) == -1) {
-        return -1;
-    }
-
-    if ((len = write(fd, buffer, buf_size)) < 0) {
-        close(fd);
-        return -1;
-    }
-
-    if ((close(fd) == -1)) {
-        return -1;
-    }
-
-    if ((len > buf_size) || (data_len != 0 && len != data_len)) {
-        return -1;
-    }
-
-    return 0;
-}
-
 int onlp_file_write_integer(char *filename, int value)
 {
     char buf[8] = {0};
     sprintf(buf, "%d", value);
 
-    return _onlp_file_write(filename, buf, (int)strlen(buf), 0);
+    return onlp_file_write((uint8_t*)buf, strlen(buf), filename);
 }
 
 int onlp_file_read_binary(char *filename, char *buffer, int buf_size, int data_len)
