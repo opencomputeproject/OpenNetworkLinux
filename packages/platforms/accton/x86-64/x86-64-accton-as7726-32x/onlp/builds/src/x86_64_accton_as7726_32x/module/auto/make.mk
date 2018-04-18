238b5491818e504cfdf35085a1c9e78ed235db9b
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2018-04-18 15:04:54 +0000

    The x86_64_as7726_32x module has been regenerated using the newmodule script.

---
+++ packages/platforms/accton/x86-64/x86-64-accton-as7726-32x/onlp/builds/src/x86_64_accton_as7726_32x/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_accton_as7726_32x Autogeneration
+#
+###############################################################################
+x86_64_accton_as7726_32x_AUTO_DEFS := module/auto/x86_64_accton_as7726_32x.yml
+x86_64_accton_as7726_32x_AUTO_DIRS := module/inc/x86_64_accton_as7726_32x module/src
+include $(BUILDER)/auto.mk
+
---
+++ packages/platforms/accton/x86-64/x86-64-accton-as7726-32x/onlp/builds/src/x86_64_accton_as7726_32x/module/src/x86_64_accton_as7726_32x_ucli.c
@@ -0,0 +1,50 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_accton_as7726_32x/x86_64_accton_as7726_32x_config.h>
+
+#if X86_64_ACCTON_AS7726_32X_CONFIG_INCLUDE_UCLI == 1
+
+#include <uCli/ucli.h>
+#include <uCli/ucli_argparse.h>
+#include <uCli/ucli_handler_macros.h>
+
+static ucli_status_t
+x86_64_accton_as7726_32x_ucli_ucli__config__(ucli_context_t* uc)
+{
+    UCLI_HANDLER_MACRO_MODULE_CONFIG(x86_64_accton_as7726_32x)
+}
+
+/* <auto.ucli.handlers.start> */
+/* <auto.ucli.handlers.end> */
+
+static ucli_module_t
+x86_64_accton_as7726_32x_ucli_module__ =
+    {
+        "x86_64_accton_as7726_32x_ucli",
+        NULL,
+        x86_64_accton_as7726_32x_ucli_ucli_handlers__,
+        NULL,
+        NULL,
+    };
+
+ucli_node_t*
+x86_64_accton_as7726_32x_ucli_node_create(void)
+{
+    ucli_node_t* n;
+    ucli_module_init(&x86_64_accton_as7726_32x_ucli_module__);
+    n = ucli_node_create("x86_64_accton_as7726_32x", NULL, &x86_64_accton_as7726_32x_ucli_module__);
+    ucli_node_subnode_add(n, ucli_module_log_node_create("x86_64_accton_as7726_32x"));
+    return n;
+}
+
+#else
+void*
+x86_64_accton_as7726_32x_ucli_node_create(void)
+{
+    return NULL;
+}
+#endif
+
