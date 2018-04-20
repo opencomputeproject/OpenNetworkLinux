958bb93e37592dbb38bdae2b8a3bc6fd3978b908
Joe Chan <chan.joe@inventec.com>
2018-04-19 23:42:08 -0700

    Update SYS_OBJECT_ID of Inventec platforms.

--- packages/platforms/inventec/x86-64/x86-64-inventec-d6254qs/platform-config/r0/src/python/x86_64_inventec_d6254qs_r0/__init__.py
+++ packages/platforms/inventec/x86-64/x86-64-inventec-d6254qs/platform-config/r0/src/python/x86_64_inventec_d6254qs_r0/__init__.py
@@ -5,7 +5,7 @@ class OnlPlatform_x86_64_inventec_d6254qs_r0(OnlPlatformInventec,
                                               OnlPlatformPortConfig_32x100):
     PLATFORM='x86-64-inventec-d6254qs-r0'
     MODEL="X86-D6254QS"
-    SYS_OBJECT_ID=".1.32"
+    SYS_OBJECT_ID="6254.10"
 
     def baseconfig(self):
         os.system("insmod /lib/modules/`uname -r`/kernel/drivers/gpio/gpio-ich.ko gpiobase=0")
