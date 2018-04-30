0e283dee3a8f4d7596dffb5a7e38472d7d33303b
Lewis Kang <kang@accton.com>
2018-04-30 08:57:23 +0800

    avoid comipiling errors. role back as5712/5812/6712/6812 to use k3.16 since their cpld drivers are not ready for k4.14 yet.

--- packages/platforms/accton/x86-64/x86-64-accton-as5812-54t/modules/builds/x86-64-accton-as5812-54t-leds.c
+++ packages/platforms/accton/x86-64/x86-64-accton-as5812-54t/modules/builds/x86-64-accton-as5812-54t-leds.c
@@ -32,11 +32,6 @@
 extern int accton_i2c_cpld_read(unsigned short cpld_addr, u8 reg);
 extern int accton_i2c_cpld_write(unsigned short cpld_addr, u8 reg, u8 value);
 
-extern void led_classdev_unregister(struct led_classdev *led_cdev);
-extern int led_classdev_register(struct device *parent, struct led_classdev *led_cdev);
-extern void led_classdev_resume(struct led_classdev *led_cdev);
-extern void led_classdev_suspend(struct led_classdev *led_cdev);
-
 #define DRVNAME "as5812_54t_led"
 
 struct accton_as5812_54t_led_data {
