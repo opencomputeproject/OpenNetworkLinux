45a08ada04d929a4483bd2c876ae78793545f3b6
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2018-05-01 20:12:24 +0000

    Add CROSS_COMPILER and ARCH_BOOT settings for each architecture.

--- make/config.armel.mk
+++ make/config.armel.mk
@@ -5,6 +5,8 @@
 ############################################################
 include $(ONL)/make/config.mk
 export TOOLCHAIN := arm-linux-gnueabi
+export CROSS_COMPILER := $(TOOLCHAIN)-
 export ARCH := armel
 export UARCH := ARMEL
+export ARCH_BOOT := uboot
 export __$(ARCH)__ := 1
