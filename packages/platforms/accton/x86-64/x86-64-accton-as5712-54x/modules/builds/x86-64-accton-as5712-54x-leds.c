76bfcf895d21e07d74ed3a4560efb22ccfc304b9
Lewis Kang <kang@accton.com>
2018-05-18 16:05:12 +0800

    as5712-54x to support kernel 4.14 (from Brandon)

--- packages/platforms/accton/x86-64/x86-64-accton-as5712-54x/modules/builds/x86-64-accton-as5712-54x-leds.c
+++ packages/platforms/accton/x86-64/x86-64-accton-as5712-54x/modules/builds/x86-64-accton-as5712-54x-leds.c
@@ -29,8 +29,8 @@
 #include <linux/leds.h>
 #include <linux/slab.h>
 
-extern int as5712_54x_i2c_cpld_read (unsigned short cpld_addr, u8 reg);
-extern int as5712_54x_i2c_cpld_write(unsigned short cpld_addr, u8 reg, u8 value);
+extern int as5712_54x_cpld_read (unsigned short cpld_addr, u8 reg);
+extern int as5712_54x_cpld_write(unsigned short cpld_addr, u8 reg, u8 value);
 
 #define DRVNAME "as5712_54x_led"
 
@@ -215,12 +215,12 @@ static u8 led_light_mode_to_reg_val(enum led_type type,
 
 static int accton_as5712_54x_led_read_value(u8 reg)
 {
-    return as5712_54x_i2c_cpld_read(0x60, reg);
+    return as5712_54x_cpld_read(0x60, reg);
 }
 
 static int accton_as5712_54x_led_write_value(u8 reg, u8 value)
 {
-    return as5712_54x_i2c_cpld_write(0x60, reg, value);
+    return as5712_54x_cpld_write(0x60, reg, value);
 }
 
 static void accton_as5712_54x_led_update(void)
@@ -547,10 +547,6 @@ static int __init accton_as5712_54x_led_init(void)
 {
     int ret;
 
-    extern int platform_accton_as5712_54x(void);
-    if(!platform_accton_as5712_54x()) {
-        return -ENODEV;
-    }
     ret = platform_driver_register(&accton_as5712_54x_led_driver);
     if (ret < 0) {
         goto exit;
