afae91cf04a9ea7fcebfe9bc94d595028b539c06
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2018-06-08 15:12:40 +0000

    Update to 4.14.48.

--- packages/base/any/kernels/4.14-lts/kconfig.mk
+++ packages/base/any/kernels/4.14-lts/kconfig.mk
@@ -21,7 +21,7 @@
 THIS_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 K_MAJOR_VERSION := 4
 K_PATCH_LEVEL := 14
-K_SUB_LEVEL := 34
+K_SUB_LEVEL := 48
 K_SUFFIX :=
 K_PATCH_DIR := $(THIS_DIR)/patches
 K_MODSYNCLIST := tools/objtool
