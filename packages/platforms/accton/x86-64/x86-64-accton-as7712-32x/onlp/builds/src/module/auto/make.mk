6de227f5a1788c8fce6dc7da6be080261c40318f
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2018-08-08 00:30:50 +0000

    Moved to as7712-32x.

--- packages/platforms/accton/x86-64/x86-64-accton-as7712-32x/onlp/builds/src/module/auto/make.mk
+++
@@ -1,9 +0,0 @@
-###############################################################################
-#
-# x86_64_accton_as7712_32x Autogeneration
-#
-###############################################################################
-x86_64_accton_as7712_32x_AUTO_DEFS := module/auto/x86_64_accton_as7712_32x.yml
-x86_64_accton_as7712_32x_AUTO_DIRS := module/inc/x86_64_accton_as7712_32x module/src
-include $(BUILDER)/auto.mk
-
