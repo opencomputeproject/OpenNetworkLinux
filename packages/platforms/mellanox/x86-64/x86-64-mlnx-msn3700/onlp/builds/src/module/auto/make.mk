5fe96a8bab3efe67bd0f8ee7af7084ad1629d3f6
Oleksandr Shamray <oleksandrs@mellanox.com>
2018-08-20 12:47:26 +0000

    Add: MAN3700 (Anaconda) system support
    Signed-off-by: Oleksandr Shamray <oleksandrs@mellanox.com>

---
+++ packages/platforms/mellanox/x86-64/x86-64-mlnx-msn3700/onlp/builds/src/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_mlnx_msn3700 Autogeneration
+#
+###############################################################################
+x86_64_mlnx_msn3700_AUTO_DEFS := module/auto/x86_64_mlnx_msn3700.yml
+x86_64_mlnx_msn3700_AUTO_DIRS := module/inc/x86_64_mlnx_msn3700 module/src
+include $(BUILDER)/auto.mk
+
