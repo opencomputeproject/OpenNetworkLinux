e28c7e5e4ccd9f4f9f5077697c4c067923aeb644
Sergey Popovich <sergey.popovich@ordnance.co>
2018-09-06 19:04:56 +0300

    treewide: Unify ARCH and KERNELs variable contents
    ONL Package Manager (tools/onlpm.py) and platform make build system
    has different idea about contents of ARCH variable:
    
       - ONLPM in PKG.yml files specifies ARCH according to Debian rules
       - Make in Makefiles specifies ARCH according to Linux Kernel rules
    
    Use Debian rules everywhere. Add corresponding translation rules in
    make/kbuild.mk.
    
    Remove ARCH part from kernel package name in KERNELS and configure it
    in place using contents of ARCH variable.
    
    This patch is prepare for upcoming changes to abstract out Linux kernel
    build artifacts from onl-kernel package and make them available for
    out of tree kernel module builds in third party software (e.g. Broadcom
    Software Development kit to build linux-kernel-bde.ko and
    linux-user-bde.ko).
    
    Signed-off-by: Sergey Popovich <sergey.popovich@ordnance.co>

--- make/kmodule.mk
+++ make/kmodule.mk
@@ -24,6 +24,20 @@ SUBDIR := "onl/$(VENDOR)/$(BASENAME)"
 
 endif
 
+KERNELS := $(KERNELS)-mbuilds:$(ARCH)
+
+ifeq ($(ARCH),amd64)
+ARCH := x86_64
+else
+ifeq ($(ARCH),armel)
+ARCH := arm
+else
+ifeq ($(ARCH),armhf)
+ARCH := arm
+endif
+endif
+endif
+
 modules:
 	rm -rf lib
 	ARCH=$(ARCH) $(ONL)/tools/scripts/kmodbuild.sh "$(KERNELS)" "$(KMODULES)" "$(SUBDIR)" "$(KINCLUDES)"
