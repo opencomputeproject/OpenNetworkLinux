b33ee3aea96c6ae024b1d29d9efef8e59770a4bd
Jostar Yang <jostar_yang@accton.com>
2018-11-13 18:13:50 +0800

    Add suport as9716-32d onlp

---
+++ packages/platforms/accton/x86-64/x86-64-accton-as9716_32d/onlp/builds/src/module/src/x86_64_accton_as9716_32d_log.c
@@ -0,0 +1,18 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_accton_as9716_32d/x86_64_accton_as9716_32d_config.h>
+
+#include "x86_64_accton_as9716_32d_log.h"
+/*
+ * x86_64_accton_as9716_32d log struct.
+ */
+AIM_LOG_STRUCT_DEFINE(
+                      X86_64_ACCTON_AS9716_32D_CONFIG_LOG_OPTIONS_DEFAULT,
+                      X86_64_ACCTON_AS9716_32D_CONFIG_LOG_BITS_DEFAULT,
+                      NULL, /* Custom log map */
+                      X86_64_ACCTON_AS9716_32D_CONFIG_LOG_CUSTOM_BITS_DEFAULT
+                     );
+
