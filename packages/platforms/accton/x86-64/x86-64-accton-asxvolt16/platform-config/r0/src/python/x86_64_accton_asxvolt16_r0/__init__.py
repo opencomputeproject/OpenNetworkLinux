eff6d274737d53da25ec0560b7bc497f0bf764e5
roy_lee <roy_lee@accton.com>
2018-12-12 10:34:35 +0800

    Change SYS_OBJECT_ID to be pure numbers. Signed-off-by: roy_lee <roy_lee@accton.com>

--- packages/platforms/accton/x86-64/x86-64-accton-asxvolt16/platform-config/r0/src/python/x86_64_accton_asxvolt16_r0/__init__.py
+++ packages/platforms/accton/x86-64/x86-64-accton-asxvolt16/platform-config/r0/src/python/x86_64_accton_asxvolt16_r0/__init__.py
@@ -5,7 +5,7 @@ class OnlPlatform_x86_64_accton_asxvolt16_r0(OnlPlatformAccton,
                                              OnlPlatformPortConfig_20x100):
     PLATFORM='x86-64-accton-asxvolt16-r0'
     MODEL="ASXvOLT16"
-    SYS_OBJECT_ID=".volt.16"
+    SYS_OBJECT_ID=".816.1"
 
     def baseconfig(self):
         self.insmod('ym2651y')
