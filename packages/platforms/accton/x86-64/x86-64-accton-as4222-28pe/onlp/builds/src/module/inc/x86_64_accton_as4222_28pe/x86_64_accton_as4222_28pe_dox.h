9efa90fb580c72acb502f8b7a487e0baa6803ba6
Jostar Yang <jostar_yang@accton.com>
2019-01-08 11:01:22 +0800

    Add to support as4222_28pe ONLP

---
+++ packages/platforms/accton/x86-64/x86-64-accton-as4222-28pe/onlp/builds/src/module/inc/x86_64_accton_as4222_28pe/x86_64_accton_as4222_28pe_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_accton_as4222_28pe Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __X86_64_ACCTON_AS4222_28PE_DOX_H__
+#define __X86_64_ACCTON_AS4222_28PE_DOX_H__
+
+/**
+ * @defgroup x86_64_accton_as4222_28pe x86_64_accton_as4222_28pe - x86_64_accton_as4222_28pe Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_accton_as4222_28pe-x86_64_accton_as4222_28pe Public Interface
+ * @defgroup x86_64_accton_as4222_28pe-config Compile Time Configuration
+ * @defgroup x86_64_accton_as4222_28pe-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __X86_64_ACCTON_AS4222_28PE_DOX_H__ */
