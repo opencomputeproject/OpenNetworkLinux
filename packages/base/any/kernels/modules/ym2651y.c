3f8142ccf42ba6ecefd075aefc419d94a2ceebfc
brandon_chuang <brandon_chuang@CicadaBuildServer.accton.com.tw>
2019-02-12 14:37:29 +0800

    [as4610] Replace hwmon_device_register() with hwmon_device_register_with_info()

--- packages/base/any/kernels/modules/ym2651y.c
+++ packages/base/any/kernels/modules/ym2651y.c
@@ -461,7 +461,8 @@ static int ym2651y_probe(struct i2c_client *client,
         goto exit_free;
     }
 
-    data->hwmon_dev = hwmon_device_register(&client->dev);
+    data->hwmon_dev = hwmon_device_register_with_info(&client->dev, "ym2651y",
+                                                      NULL, NULL, NULL);
     if (IS_ERR(data->hwmon_dev)) {
         status = PTR_ERR(data->hwmon_dev);
         goto exit_remove;
