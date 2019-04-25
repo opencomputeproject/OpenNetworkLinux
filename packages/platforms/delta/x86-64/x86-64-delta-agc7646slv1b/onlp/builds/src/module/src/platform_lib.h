d52ed2e9eebfa58cb9db3e66d64c7941f1d650c4
johnson <JOHNSON.LU@deltaww.com>
2019-04-25 16:05:07 +0800

    modify platform_lib.h
    Signed-off-by: johnson <JOHNSON.LU@deltaww.com>

--- packages/platforms/delta/x86-64/x86-64-delta-agc7646slv1b/onlp/builds/src/module/src/platform_lib.h
+++ packages/platforms/delta/x86-64/x86-64-delta-agc7646slv1b/onlp/builds/src/module/src/platform_lib.h
@@ -56,14 +56,11 @@ typedef unsigned int    UINT4;
 #define PSU_EEPROM_TIME_THRESHOLD    (10)
 #define SWPLD_DATA_TIME_THRESHOLD    (5)
 #define DEV_NUM                      (30)
-<<<<<<< HEAD
 #define SWPLD_NUM                    (3)
-=======
->>>>>>> 5e928be2867e253b53fe461779f5113acd3c414b
 
 #define CPU_CPLD_VERSION "/sys/devices/platform/delta-agc7646slv1b-cpld.0/cpuld_ver"
 #define IDPROM_PATH "/sys/class/i2c-adapter/i2c-1/1-0053/eeprom"
-#define PORT_EEPROM_FORMAT      "/sys/bus/i2c/devices/%d-0050/eeprom"
+#define PORT_EEPROM_FORMAT "/sys/bus/i2c/devices/%d-0050/eeprom"
 #define CHECK_TIME_FILE "/tmp/check_time_file"
 #define BMC_INFO_TABLE "/tmp/bmc_info"
 
