8251be6564d24165dd168f75410a4d351ece0752
Oleksandr Shamray <oleksandrs@mellanox.com>
2019-08-16 11:31:13 +0000

    Clear redundant folders afrer simplify the platform directory names by Jeffery

--- packages/platforms/mellanox/x86-64/x86-64-mlnx-msn3800/onlp/builds/src/module/auto/make.mk
+++
@@ -1,9 +0,0 @@
-###############################################################################
-#
-# x86_64_mlnx_msn3800 Autogeneration
-#
-###############################################################################
-x86_64_mlnx_msn3800_AUTO_DEFS := module/auto/x86_64_mlnx_msn3800.yml
-x86_64_mlnx_msn3800_AUTO_DIRS := module/inc/x86_64_mlnx_msn3800 module/src
-include $(BUILDER)/auto.mk
-
