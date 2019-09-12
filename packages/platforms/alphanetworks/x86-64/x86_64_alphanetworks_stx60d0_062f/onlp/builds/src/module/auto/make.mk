da3027e34b0b32d4e89f149343bd1da5689210f2
cfeng111 <cfeng111.cc@gmail.com>
2019-09-12 15:56:21 +0800

    remove x86_64_alphanetworks_stx60d0_062f

--- packages/platforms/alphanetworks/x86-64/x86_64_alphanetworks_stx60d0_062f/onlp/builds/src/module/auto/make.mk
+++
@@ -1,9 +0,0 @@
-###############################################################################
-#
-# x86_64_alphanetworks_stx60d0_062f Autogeneration
-#
-###############################################################################
-x86_64_alphanetworks_stx60d0_062f_AUTO_DEFS := module/auto/x86_64_alphanetworks_stx60d0_062f.yml
-x86_64_alphanetworks_stx60d0_062f_AUTO_DIRS := module/inc/x86_64_alphanetworks_stx60d0_062f module/src
-include $(BUILDER)/auto.mk
-
