28c3ee7b0bb073a93a3e1277c691f83b69a11f25
brandon_chuang <brandon_chuang@CicadaBuildServer.accton.com.tw>
2019-10-01 16:24:48 +0800

    [as5835_54t] Add make.mk

---
+++ packages/platforms/accton/x86-64/as5835-54t/onlp/builds/x86_64_accton_as5835_54t/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_accton_as5835_54t Autogeneration
+#
+###############################################################################
+x86_64_accton_as5835_54t_AUTO_DEFS := module/auto/x86_64_accton_as5835_54t.yml
+x86_64_accton_as5835_54t_AUTO_DIRS := module/inc/x86_64_accton_as5835_54t module/src
+include $(BUILDER)/auto.mk
+
