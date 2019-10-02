af0f45c6867542779d3b81afad36bd76def13fee
Mykola Kostenok <c_mykolak@mellanox.com>
2019-10-02 18:10:01 +0300

    add new system MSN3420.
    Signed-off-by: Mykola Kostenok <c_mykolak@mellanox.com>

---
+++ packages/platforms/mellanox/x86-64/msn3420/onlp/builds/x86_64_mlnx_msn3420/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_mlnx_msn3420 Autogeneration
+#
+###############################################################################
+x86_64_mlnx_msn3420_AUTO_DEFS := module/auto/x86_64_mlnx_msn3420.yml
+x86_64_mlnx_msn3420_AUTO_DIRS := module/inc/x86_64_mlnx_msn3420 module/src
+include $(BUILDER)/auto.mk
+
---
+++ packages/platforms/mellanox/x86-64/msn3420/onlp/builds/x86_64_mlnx_msn3420/module/inc/x86_64_mlnx_msn3420/x86_64_mlnx_msn3420_porting.h
@@ -0,0 +1,97 @@
+/**************************************************************************//**
+ *
+ * @file
+ * @brief x86_64_mlnx_msn3420 Porting Macros.
+ *
+ * @addtogroup x86_64_mlnx_msn3420-porting
+ * @{
+ *
+ *****************************************************************************/
+#ifndef __X86_64_MLNX_MSN3420_PORTING_H__
+#define __X86_64_MLNX_MSN3420_PORTING_H__
+
+
+/* <auto.start.portingmacro(ALL).define> */
+#if X86_64_MLNX_MSN3420_CONFIG_PORTING_INCLUDE_STDLIB_HEADERS == 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <memory.h>
+#endif
+
+#ifndef X86_64_MLNX_MSN3420_MALLOC
+    #if defined(GLOBAL_MALLOC)
+        #define X86_64_MLNX_MSN3420_MALLOC GLOBAL_MALLOC
+    #elif X86_64_MLNX_MSN3420_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_MLNX_MSN3420_MALLOC malloc
+    #else
+        #error The macro X86_64_MLNX_MSN3420_MALLOC is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_MLNX_MSN3420_FREE
+    #if defined(GLOBAL_FREE)
+        #define X86_64_MLNX_MSN3420_FREE GLOBAL_FREE
+    #elif X86_64_MLNX_MSN3420_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_MLNX_MSN3420_FREE free
+    #else
+        #error The macro X86_64_MLNX_MSN3420_FREE is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_MLNX_MSN3420_MEMSET
+    #if defined(GLOBAL_MEMSET)
+        #define X86_64_MLNX_MSN3420_MEMSET GLOBAL_MEMSET
+    #elif X86_64_MLNX_MSN3420_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_MLNX_MSN3420_MEMSET memset
+    #else
+        #error The macro X86_64_MLNX_MSN3420_MEMSET is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_MLNX_MSN3420_MEMCPY
+    #if defined(GLOBAL_MEMCPY)
+        #define X86_64_MLNX_MSN3420_MEMCPY GLOBAL_MEMCPY
+    #elif X86_64_MLNX_MSN3420_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_MLNX_MSN3420_MEMCPY memcpy
+    #else
+        #error The macro X86_64_MLNX_MSN3420_MEMCPY is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_MLNX_MSN3420_VSNPRINTF
+    #if defined(GLOBAL_VSNPRINTF)
+        #define X86_64_MLNX_MSN3420_VSNPRINTF GLOBAL_VSNPRINTF
+    #elif X86_64_MLNX_MSN3420_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_MLNX_MSN3420_VSNPRINTF vsnprintf
+    #else
+        #error The macro X86_64_MLNX_MSN3420_VSNPRINTF is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_MLNX_MSN3420_SNPRINTF
+    #if defined(GLOBAL_SNPRINTF)
+        #define X86_64_MLNX_MSN3420_SNPRINTF GLOBAL_SNPRINTF
+    #elif X86_64_MLNX_MSN3420_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_MLNX_MSN3420_SNPRINTF snprintf
+    #else
+        #error The macro X86_64_MLNX_MSN3420_SNPRINTF is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_MLNX_MSN3420_STRLEN
+    #if defined(GLOBAL_STRLEN)
+        #define X86_64_MLNX_MSN3420_STRLEN GLOBAL_STRLEN
+    #elif X86_64_MLNX_MSN3420_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_MLNX_MSN3420_STRLEN strlen
+    #else
+        #error The macro X86_64_MLNX_MSN3420_STRLEN is required but cannot be defined.
+    #endif
+#endif
+
+/* <auto.end.portingmacro(ALL).define> */
+
+
+#endif /* __X86_64_MLNX_MSN3420_PORTING_H__ */
+/* @} */
