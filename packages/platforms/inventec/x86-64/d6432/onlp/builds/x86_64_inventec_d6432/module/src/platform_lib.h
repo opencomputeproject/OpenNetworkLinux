b71aa7b852907ed97bf1b8fb5a0e22ace157d6ce
cynthia <wu.cynthia@inventec.com>
2020-01-07 11:37:55 +0800

    Support 400G for ONLP and update accroding to new SWPS driver on platform Inventec D6432/D7332

--- packages/platforms/inventec/x86-64/d6432/onlp/builds/x86_64_inventec_d6432/module/src/platform_lib.h
+++ packages/platforms/inventec/x86-64/d6432/onlp/builds/x86_64_inventec_d6432/module/src/platform_lib.h
@@ -54,7 +54,7 @@
 #define INV_FAN_PREFIX                  "/sys/bus/i2c/devices/2-0077/"
 #define INV_THERMAL_PREFIX              "/sys/bus/i2c/devices/2-0077/"
 #define INV_LED_PREFIX                  "/sys/bus/i2c/devices/2-0077/"
-#define INV_SFP_PREFIX		            "/sys/swps/sff/"
+#define INV_SFP_PREFIX		            "/sys/swps/card1/sff/"
 #define INV_SYS_PREFIX		            "/sys/class/eeprom/vpd/"
 #define INV_DEVICE_BASE                    "/sys/bus/i2c/devices/"
 #define INV_CTMP_BASE                   "/sys/devices/platform/coretemp.0/hwmon/"
