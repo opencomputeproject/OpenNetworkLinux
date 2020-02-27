e64fc079d85cf667ea1371ef224d3a2a15d332b8
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2020-02-27 19:26:58 +0000

    The modules must disable warnings as part of the BROKEN_CFLAGS feature to compile with GCC in buster.
    The problems reported are legitimate and should be resolved by the owner.

--- packages/platforms/inventec/x86-64/d3352/onlp/builds/x86_64_inventec_d3352/module/make.mk
+++ packages/platforms/inventec/x86-64/d3352/onlp/builds/x86_64_inventec_d3352/module/make.mk
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# 
+#
 #
 ###############################################################################
 THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
@@ -8,3 +8,4 @@ x86_64_inventec_d3352_INCLUDES := -I $(THIS_DIR)inc
 x86_64_inventec_d3352_INTERNAL_INCLUDES := -I $(THIS_DIR)src
 x86_64_inventec_d3352_DEPENDMODULE_ENTRIES := init:x86_64_inventec_d3352 ucli:x86_64_inventec_d3352
 
+x86_64_inventec_d3352_BROKEN_CFLAGS += -Wno-stringop-truncation -Wno-restrict -Wno-format-truncation
--- packages/platforms/inventec/x86-64/d6356/onlp/builds/x86_64_inventec_d6356/module/make.mk
+++ packages/platforms/inventec/x86-64/d6356/onlp/builds/x86_64_inventec_d6356/module/make.mk
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# 
+#
 #
 ###############################################################################
 THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
@@ -8,3 +8,5 @@ x86_64_inventec_d6356_INCLUDES := -I $(THIS_DIR)inc
 x86_64_inventec_d6356_INTERNAL_INCLUDES := -I $(THIS_DIR)src
 x86_64_inventec_d6356_DEPENDMODULE_ENTRIES := init:x86_64_inventec_d6356 ucli:x86_64_inventec_d6356
 
+# This error should be addressed in the code.
+x86_64_inventec_d6356_BROKEN_CFLAGS += -Wno-stringop-truncation
