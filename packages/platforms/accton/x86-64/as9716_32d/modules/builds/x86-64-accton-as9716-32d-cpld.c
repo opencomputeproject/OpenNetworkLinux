23b6ee212866279bfcb783eaa0068d04dba7fe2f
Jostar Yang <jostar_yang@accton.com>
2020-03-13 11:46:42 +0800

    [as9716-32d]:Add to support qsfp-dd reset sysfs

--- packages/platforms/accton/x86-64/as9716_32d/modules/builds/x86-64-accton-as9716-32d-cpld.c
+++ packages/platforms/accton/x86-64/as9716_32d/modules/builds/x86-64-accton-as9716-32d-cpld.c
@@ -69,8 +69,9 @@ MODULE_DEVICE_TABLE(i2c, as9716_32d_cpld_id);
 
 #define TRANSCEIVER_PRESENT_ATTR_ID(index)   	MODULE_PRESENT_##index
 #define TRANSCEIVER_TXDISABLE_ATTR_ID(index)   	MODULE_TXDISABLE_##index
-#define TRANSCEIVER_RXLOS_ATTR_ID(index)   		MODULE_RXLOS_##index
+#define TRANSCEIVER_RXLOS_ATTR_ID(index)   	MODULE_RXLOS_##index
 #define TRANSCEIVER_TXFAULT_ATTR_ID(index)   	MODULE_TXFAULT_##index
+#define TRANSCEIVER_RESET_ATTR_ID(index)        MODULE_RESET_##index
 
 enum as9716_32d_cpld_sysfs_attributes {
 	CPLD_VERSION,
@@ -115,7 +116,39 @@ enum as9716_32d_cpld_sysfs_attributes {
 	TRANSCEIVER_RXLOS_ATTR_ID(33),
 	TRANSCEIVER_RXLOS_ATTR_ID(34),
 	TRANSCEIVER_TXFAULT_ATTR_ID(33),
-	TRANSCEIVER_TXFAULT_ATTR_ID(34)
+	TRANSCEIVER_TXFAULT_ATTR_ID(34),
+	TRANSCEIVER_RESET_ATTR_ID(1),
+	TRANSCEIVER_RESET_ATTR_ID(2),
+	TRANSCEIVER_RESET_ATTR_ID(3),
+	TRANSCEIVER_RESET_ATTR_ID(4),
+	TRANSCEIVER_RESET_ATTR_ID(5),
+	TRANSCEIVER_RESET_ATTR_ID(6),
+	TRANSCEIVER_RESET_ATTR_ID(7),
+	TRANSCEIVER_RESET_ATTR_ID(8),
+	TRANSCEIVER_RESET_ATTR_ID(9),
+	TRANSCEIVER_RESET_ATTR_ID(10),
+	TRANSCEIVER_RESET_ATTR_ID(11),
+	TRANSCEIVER_RESET_ATTR_ID(12),
+	TRANSCEIVER_RESET_ATTR_ID(13),
+	TRANSCEIVER_RESET_ATTR_ID(14),
+	TRANSCEIVER_RESET_ATTR_ID(15),
+	TRANSCEIVER_RESET_ATTR_ID(16),
+	TRANSCEIVER_RESET_ATTR_ID(17),
+	TRANSCEIVER_RESET_ATTR_ID(18),
+	TRANSCEIVER_RESET_ATTR_ID(19),
+	TRANSCEIVER_RESET_ATTR_ID(20),
+	TRANSCEIVER_RESET_ATTR_ID(21),
+	TRANSCEIVER_RESET_ATTR_ID(22),
+	TRANSCEIVER_RESET_ATTR_ID(23),
+	TRANSCEIVER_RESET_ATTR_ID(24),
+	TRANSCEIVER_RESET_ATTR_ID(25),
+	TRANSCEIVER_RESET_ATTR_ID(26),
+	TRANSCEIVER_RESET_ATTR_ID(27),
+	TRANSCEIVER_RESET_ATTR_ID(28),
+	TRANSCEIVER_RESET_ATTR_ID(29),
+	TRANSCEIVER_RESET_ATTR_ID(30),
+	TRANSCEIVER_RESET_ATTR_ID(31),
+	TRANSCEIVER_RESET_ATTR_ID(32),	
 };
 
 /* sysfs attributes for hwmon 
@@ -123,6 +156,8 @@ enum as9716_32d_cpld_sysfs_attributes {
 static ssize_t show_status(struct device *dev, struct device_attribute *da,
              char *buf);
 static ssize_t set_tx_disable(struct device *dev, struct device_attribute *da,
+			const char *buf, size_t count);
+static ssize_t set_port_reset(struct device *dev, struct device_attribute *da,
 			const char *buf, size_t count);
 static ssize_t access(struct device *dev, struct device_attribute *da,
 			const char *buf, size_t count);
@@ -133,15 +168,20 @@ static int as9716_32d_cpld_write_internal(struct i2c_client *client, u8 reg, u8 
 
 /* transceiver attributes */
 #define DECLARE_TRANSCEIVER_PRESENT_SENSOR_DEVICE_ATTR(index) \
-	static SENSOR_DEVICE_ATTR(module_present_##index, S_IRUGO, show_status, NULL, MODULE_PRESENT_##index)
-#define DECLARE_TRANSCEIVER_PRESENT_ATTR(index)  &sensor_dev_attr_module_present_##index.dev_attr.attr
+	static SENSOR_DEVICE_ATTR(module_present_##index, S_IRUGO, show_status, NULL, MODULE_PRESENT_##index); \
+	static SENSOR_DEVICE_ATTR(module_reset_##index, S_IRUGO | S_IWUSR, show_status, set_port_reset, MODULE_RESET_##index);
+#define DECLARE_TRANSCEIVER_PRESENT_ATTR(index) \
+   &sensor_dev_attr_module_present_##index.dev_attr.attr, \
+   &sensor_dev_attr_module_reset_##index.dev_attr.attr
 
 #define DECLARE_SFP_TRANSCEIVER_SENSOR_DEVICE_ATTR(index) \
+    static SENSOR_DEVICE_ATTR(module_present_##index, S_IRUGO, show_status, NULL, MODULE_PRESENT_##index); \
 	static SENSOR_DEVICE_ATTR(module_tx_disable_##index, S_IRUGO | S_IWUSR, show_status, set_tx_disable, MODULE_TXDISABLE_##index); \
 	static SENSOR_DEVICE_ATTR(module_rx_los_##index, S_IRUGO, show_status, NULL, MODULE_RXLOS_##index);  \
 	static SENSOR_DEVICE_ATTR(module_tx_fault_##index, S_IRUGO, show_status, NULL, MODULE_RXLOS_##index); 
 	
 #define DECLARE_SFP_TRANSCEIVER_ATTR(index)  \
+    &sensor_dev_attr_module_present_##index.dev_attr.attr, \
 	&sensor_dev_attr_module_tx_disable_##index.dev_attr.attr, \
 	&sensor_dev_attr_module_rx_los_##index.dev_attr.attr,     \
 	&sensor_dev_attr_module_tx_fault_##index.dev_attr.attr
@@ -182,8 +222,6 @@ DECLARE_TRANSCEIVER_PRESENT_SENSOR_DEVICE_ATTR(29);
 DECLARE_TRANSCEIVER_PRESENT_SENSOR_DEVICE_ATTR(30);
 DECLARE_TRANSCEIVER_PRESENT_SENSOR_DEVICE_ATTR(31);
 DECLARE_TRANSCEIVER_PRESENT_SENSOR_DEVICE_ATTR(32);
-DECLARE_TRANSCEIVER_PRESENT_SENSOR_DEVICE_ATTR(33);
-DECLARE_TRANSCEIVER_PRESENT_SENSOR_DEVICE_ATTR(34);
 DECLARE_SFP_TRANSCEIVER_SENSOR_DEVICE_ATTR(33);
 DECLARE_SFP_TRANSCEIVER_SENSOR_DEVICE_ATTR(34);
 
@@ -228,26 +266,24 @@ static struct attribute *as9716_32d_cpld2_attributes[] = {
     &sensor_dev_attr_version.dev_attr.attr,
     &sensor_dev_attr_access.dev_attr.attr,	
     DECLARE_TRANSCEIVER_PRESENT_ATTR(17),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(18),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(19),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(20),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(21),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(22),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(23),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(24),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(25),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(26),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(27),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(28),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(29),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(30),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(31),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(32),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(33),
-	DECLARE_TRANSCEIVER_PRESENT_ATTR(34),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(18),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(19),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(20),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(21),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(22),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(23),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(24),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(25),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(26),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(27),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(28),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(29),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(30),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(31),
+    DECLARE_TRANSCEIVER_PRESENT_ATTR(32),
     DECLARE_SFP_TRANSCEIVER_ATTR(33),
     DECLARE_SFP_TRANSCEIVER_ATTR(34),
-	NULL
+    NULL
 };
 
 static const struct attribute_group as9716_32d_cpld2_group = {
@@ -306,11 +342,30 @@ static ssize_t show_status(struct device *dev, struct device_attribute *da,
 		mask = 0x2;
 		break;	
 	
-	default:
-		return 0;
-	}
+        case MODULE_RESET_1 ... MODULE_RESET_8:
+            reg  = 0x14;
+            mask = 0x1 << (attr->index - MODULE_RESET_1);
+            break;
+        case MODULE_RESET_9 ... MODULE_RESET_16:
+            reg  = 0x15;
+            mask = 0x1 << (attr->index - MODULE_RESET_9);
+            break;
+        case MODULE_RESET_17 ... MODULE_RESET_24:
+            reg  = 0x14;
+            mask = 0x1 << (attr->index - MODULE_RESET_17);
+            break;
+        case MODULE_RESET_25 ... MODULE_RESET_32:
+            reg  = 0x15;
+            mask = 0x1 << (attr->index - MODULE_RESET_25);
+            break;
+        default:
+            return 0;
+    }
 
     if (attr->index >= MODULE_PRESENT_1 && attr->index <= MODULE_PRESENT_34) {
+        revert = 1;
+    }
+    if (attr->index >= MODULE_RESET_1 && attr->index <= MODULE_RESET_32) {
         revert = 1;
     }
 
@@ -383,6 +438,72 @@ exit:
 	mutex_unlock(&data->update_lock);
 	return status;
 }
+
+static ssize_t set_port_reset(struct device *dev, struct device_attribute *da,
+			const char *buf, size_t count)
+{
+    struct sensor_device_attribute *attr = to_sensor_dev_attr(da);
+    struct i2c_client *client = to_i2c_client(dev);
+    struct as9716_32d_cpld_data *data = i2c_get_clientdata(client);
+    long reset;
+    int status;
+    u8 reg = 0, mask = 0;
+
+    status = kstrtol(buf, 10, &reset);
+    if (status) {
+        return status;
+    }
+
+    switch (attr->index)
+    {
+        case MODULE_RESET_1 ... MODULE_RESET_8:
+            reg  = 0x14;
+	    mask = 0x1 << (attr->index - MODULE_RESET_1);
+            break;
+        case MODULE_RESET_9 ... MODULE_RESET_16:
+            reg  = 0x15;
+            mask = 0x1 << (attr->index - MODULE_RESET_9);
+            break;
+        case MODULE_RESET_17 ... MODULE_RESET_24:
+            reg  = 0x14;
+            mask = 0x1 << (attr->index - MODULE_RESET_17);
+            break;
+        case MODULE_RESET_25 ... MODULE_RESET_32:
+            reg  = 0x15;
+            mask = 0x1 << (attr->index - MODULE_RESET_25);
+            break;
+        default:
+            return -ENXIO;
+    }
+    
+    /* Read current status */
+    mutex_lock(&data->update_lock);
+    status = as9716_32d_cpld_read_internal(client, reg);
+    if (unlikely(status < 0)) {
+        goto exit;
+    }
+
+    /* Update tx_disable status */
+    if (reset) {
+        status &= ~mask;
+    }
+    else {
+        status |= mask;
+    }
+
+    status = as9716_32d_cpld_write_internal(client, reg, status);
+    if (unlikely(status < 0)) {
+        goto exit;
+    }
+    
+    mutex_unlock(&data->update_lock);
+    return count;
+
+exit:
+    mutex_unlock(&data->update_lock);
+    return status;
+}
+
 
 static ssize_t access(struct device *dev, struct device_attribute *da,
 			const char *buf, size_t count)
