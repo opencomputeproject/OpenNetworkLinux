5e65981ed175397626fc23d227a552ba6c4b27b6
Oleksandr Shamray <oleksandrs@mellanox.com>
2020-03-17 10:11:48 +0200

    Added Mellanox MSN4600(64x200G)/MSN4600C(64x100G) systems
    Signed-off-by: Oleksandr Shamray <oleksandrs@mellanox.com>

---
+++ packages/platforms/mellanox/x86-64/msn4600/onlp/builds/x86_64_mlnx_msn4600/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_mlnx_msn4600 Autogeneration
+#
+###############################################################################
+x86_64_mlnx_msn4600_AUTO_DEFS := module/auto/x86_64_mlnx_msn4600.yml
+x86_64_mlnx_msn4600_AUTO_DIRS := module/inc/x86_64_mlnx_msn4600 module/src
+include $(BUILDER)/auto.mk
+
