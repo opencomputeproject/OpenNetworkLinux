6fa987980cb8cf3639c14443cb2d55f8814ce4d8
Andrew Sapronov <andrew.sapronov@gmail.com>
2020-04-24 03:59:38 +0800

    Netberg Aurora 750 initial support

---
+++ packages/platforms/netberg/x86-64/aurora-750/onlp/builds/x86_64_netberg_aurora_750/module/auto/make.mk
@@ -0,0 +1,7 @@
+#
+# x86_64_netberg_aurora_750 Autogeneration
+#
+###############################################################################
+x86-64-netberg-aurora-750_AUTO_DEFS := module/auto/x86-64-netberg-aurora-750.yml
+x86-64-netberg-aurora-750_AUTO_DIRS := module/inc/x86-64-netberg-aurora-750 module/src
+include $(BUILDER)/auto.mk
---
+++ packages/platforms/netberg/x86-64/aurora-750/onlp/builds/x86_64_netberg_aurora_750/module/inc/x86_64_netberg_aurora_750/x86_64_netberg_aurora_750_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_netberg_aurora_750 Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __x86_64_netberg_aurora_750_DOX_H__
+#define __x86_64_netberg_aurora_750_DOX_H__
+
+/**
+ * @defgroup x86_64_netberg_aurora_750 x86_64_netberg_aurora_750 - x86_64_netberg_aurora_750 Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_netberg_aurora_750-x86_64_netberg_aurora_750 Public Interface
+ * @defgroup x86_64_netberg_aurora_750-config Compile Time Configuration
+ * @defgroup x86_64_netberg_aurora_750-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __x86_64_netberg_aurora_750_DOX_H__ */
