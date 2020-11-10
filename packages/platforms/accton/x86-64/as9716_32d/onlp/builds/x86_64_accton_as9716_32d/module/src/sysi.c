5a3a89c555fd4b71a69746e3a2b2ef07b0feac73
Jostar Yang <jostar_yang@accton.com.tw>
2020-11-10 16:57:04 +0800

    [as9716-32d]Modify '100% speed' to 'full speed
    Signed-off-by: Jostar Yang <jostar_yang@accton.com.tw>

--- packages/platforms/accton/x86-64/as9716_32d/onlp/builds/x86_64_accton_as9716_32d/module/src/sysi.c
+++ packages/platforms/accton/x86-64/as9716_32d/onlp/builds/x86_64_accton_as9716_32d/module/src/sysi.c
@@ -468,7 +468,7 @@ int onlp_sysi_platform_manage_fans(void)
     {
         if (onlp_thermali_info_get(ONLP_THERMAL_ID_CREATE(i), &thermal[k]) != ONLP_STATUS_OK  )
         {
-            AIM_LOG_ERROR("Unable to read thermal status, set fans to 100% speed");
+            AIM_LOG_ERROR("Unable to read thermal status, set fans to full speed");
             onlp_fani_percentage_set(ONLP_FAN_ID_CREATE(1), 100);
             return ONLP_STATUS_E_INTERNAL;
        }
@@ -478,7 +478,7 @@ int onlp_sysi_platform_manage_fans(void)
     {
         if (onlp_thermali_info_get(ONLP_THERMAL_ID_CREATE(i), &thermal[k]) != ONLP_STATUS_OK  )
         {
-            AIM_LOG_ERROR("Unable to read thermal status, set fans to 100% speed");
+            AIM_LOG_ERROR("Unable to read thermal status, set fans to full speed");
             onlp_fani_percentage_set(ONLP_FAN_ID_CREATE(1), 100);
             return ONLP_STATUS_E_INTERNAL;
         }
@@ -486,13 +486,13 @@ int onlp_sysi_platform_manage_fans(void)
     }
     if (onlp_thermali_info_get(ONLP_THERMAL_ID_CREATE(1), &thermal[6]) != ONLP_STATUS_OK  )
     {
-        AIM_LOG_ERROR("Unable to read thermal status, set fans to 100% speed");
+        AIM_LOG_ERROR("Unable to read thermal status, set fans to full speed");
         onlp_fani_percentage_set(ONLP_FAN_ID_CREATE(1), 100);
         return ONLP_STATUS_E_INTERNAL;
     }
     if (onlp_thermali_info_get(ONLP_THERMAL_ID_CREATE(5), &thermal[7]) != ONLP_STATUS_OK  )
     {
-        AIM_LOG_ERROR("Unable to read thermal status, set fans to 100% speed");
+        AIM_LOG_ERROR("Unable to read thermal status, set fans to  full speed");
         onlp_fani_percentage_set(ONLP_FAN_ID_CREATE(1), 100);
         return ONLP_STATUS_E_INTERNAL;
     }
