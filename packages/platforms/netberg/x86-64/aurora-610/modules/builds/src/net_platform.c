0d32ea3085854c673f47f4a2b2e0df79459d6570
Andrew Sapronov <andrew.sapronov@gmail.com>
2020-11-25 12:41:09 +0300

    Improved i2c polling
      - Added new mux;
      - More  accurate i2c working mode using i2c_new_probed_device
    
    Signed-off-by: Andrew Sapronov <andrew.sapronov@gmail.com>

--- packages/platforms/netberg/x86-64/aurora-610/modules/builds/src/net_platform.c
+++ packages/platforms/netberg/x86-64/aurora-610/modules/builds/src/net_platform.c
@@ -13,6 +13,7 @@ struct net_i2c_board_info {
     int ch;
     int size;
     struct i2c_board_info *board_info;
+    int probe;
 };
 
 #define bus_id(id)  (id)
@@ -136,27 +137,31 @@ static struct i2c_board_info i2c_device_info8[] __initdata = {
 
 
 static struct net_i2c_board_info i2cdev_list[] = {
-    {bus_id(1), ARRAY_SIZE(i2c_device_info0),  i2c_device_info0 },  //mux root
-     
-    {bus_id(2), ARRAY_SIZE(i2c_device_info2),  i2c_device_info2 },  //mux 0
-    {bus_id(3), ARRAY_SIZE(i2c_device_info3),  i2c_device_info3 },  //mux 1
-    {bus_id(4), ARRAY_SIZE(i2c_device_info4),  i2c_device_info4 },  //mux 2
-    {bus_id(5), ARRAY_SIZE(i2c_device_info5),  i2c_device_info5 },  //mux 3
-    {bus_id(6), ARRAY_SIZE(i2c_device_info6),  i2c_device_info6 },  //mux 4
-    {bus_id(7), ARRAY_SIZE(i2c_device_info7),  i2c_device_info7 },  //mux 5
-    {bus_id(8), ARRAY_SIZE(i2c_device_info8),  i2c_device_info8 },  //mux 6  
+    {bus_id(0), ARRAY_SIZE(i2c_device_info0),  i2c_device_info0, 1},  //mux root
+    {bus_id(1), ARRAY_SIZE(i2c_device_info0),  i2c_device_info0, 1},  //mux root
+    {bus_id(2), ARRAY_SIZE(i2c_device_info2),  i2c_device_info2, 0},  //mux 0
+    {bus_id(3), ARRAY_SIZE(i2c_device_info3),  i2c_device_info3, 0},  //mux 1
+    {bus_id(4), ARRAY_SIZE(i2c_device_info4),  i2c_device_info4, 0},  //mux 2
+    {bus_id(5), ARRAY_SIZE(i2c_device_info5),  i2c_device_info5, 0},  //mux 3
+    {bus_id(6), ARRAY_SIZE(i2c_device_info6),  i2c_device_info6, 0},  //mux 4
+    {bus_id(7), ARRAY_SIZE(i2c_device_info7),  i2c_device_info7, 0},  //mux 5
+    {bus_id(8), ARRAY_SIZE(i2c_device_info8),  i2c_device_info8, 0},  //mux 6  
 };
+
+#define NET_PLATFORM_CLIENT_MAX_NUM 100 /*A big enough number for sum of i2cdev_list[i].size */
+static int client_list_index = 0;
+static struct i2c_client *client_list[NET_PLATFORM_CLIENT_MAX_NUM] = {0};
 
 /////////////////////////////////////////////////////////////////////////////////////////
 static struct   platform_device         *device_i2c_gpio0;
 static struct 	i2c_gpio_platform_data 	i2c_gpio_platdata0 = {
-	.scl_pin = GPIO_BASE + 58,
-	.sda_pin = GPIO_BASE + 75,
-    
-	.udelay  = 5, //5:100kHz
-	.sda_is_open_drain = 0,
-	.scl_is_open_drain = 0,
-	.scl_is_output_only = 0
+    .scl_pin = GPIO_BASE + 58,
+    .sda_pin = GPIO_BASE + 75,
+
+    .udelay  = 5, //5:100kHz
+    .sda_is_open_drain = 0,
+    .scl_is_open_drain = 0,
+    .scl_is_output_only = 0
 };
 
 static int __init net_platform_init(void)
@@ -191,28 +196,52 @@ static int __init net_platform_init(void)
     if (ret) {
         printk(KERN_ERR "i2c-gpio: platform_device_add fail %d\n", ret);
     }
+    msleep(10);
 
-   for(i=0; i<ARRAY_SIZE(i2cdev_list); i++) {
+    for(i=0; i<ARRAY_SIZE(i2cdev_list); i++) {
         adap = i2c_get_adapter( i2cdev_list[i].ch );
         if (adap == NULL) {
             printk("platform get channel %d adapter fail\n", i);
             continue;
         }
-        i2c_put_adapter(adap);
+
         for(j=0; j<i2cdev_list[i].size; j++) {
             for(k=0; k<300; k++) {
-                e = i2c_new_device(adap, &i2cdev_list[i].board_info[j] );
-                if(e == NULL) msleep(10); else break;
+                if (i2cdev_list[i].probe == 1) {
+                    short unsigned int i2c_address[2]={i2cdev_list[i].board_info[j].addr, I2C_CLIENT_END};
+                    e = i2c_new_probed_device(adap, &i2cdev_list[i].board_info[j] ,i2c_address, NULL);
+                } else {
+                    e = i2c_new_device(adap, &i2cdev_list[i].board_info[j]);
+                }
+
+                if(e == NULL) {
+                    msleep(10);
+                } else {
+                    client_list[client_list_index] = e;
+                    client_list_index++;
+                    break;
+                }
+            }
+            if(k==300) {
+                printk("[%d][%d] i2c device load fail\n",i,j);
             }
         }
+        i2c_put_adapter(adap);
     }
     return ret;    
 }
 
 static void __exit net_platform_exit(void)
 {
-	device_i2c_gpio0->dev.platform_data = NULL;
-	platform_device_unregister(device_i2c_gpio0);
+    int i;
+
+    for(i=client_list_index-1; i>=0; i--) {
+        i2c_unregister_device(client_list[i]);
+    }
+
+    device_i2c_gpio0->dev.platform_data = NULL;
+    platform_device_unregister(device_i2c_gpio0);
+    printk("net_platform_exit done\n");
 }
 
 module_init(net_platform_init);
