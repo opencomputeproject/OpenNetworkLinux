318515d421b9b5ac866f5c061d1510136dbcf6bd
Brandon Chuang <brandon_chuang@edge-core.com>
2020-12-11 02:06:01 +0000

    [as4630_54pe] Fix platform port config
    The correct port configuration of as4630_54pe is as below:
    root@localhost:~# onl-platform-show
    Model: AS4630-54PE
    Manufacturer: Accton
    Ports: 54 (48x1 + 4x25 + 2x100)
    
    Signed-off-by: Brandon Chuang <brandon_chuang@edge-core.com>

--- packages/base/all/vendor-config-onl/src/python/onl/platform/base.py
+++ packages/base/all/vendor-config-onl/src/python/onl/platform/base.py
@@ -612,3 +612,7 @@ class OnlPlatformPortConfig_6x400_48x50(object):
 class OnlPlatformPortConfig_10x400_18x100_2x10(object):
     PORT_COUNT=30
     PORT_CONFIG="10x400 + 18x100 + 2x10"
+
+class OnlPlatformPortConfig_48x1_4x25_2x100(object):
+    PORT_COUNT=54
+    PORT_CONFIG="48x1 + 4x25 + 2x100"
