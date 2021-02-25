4da10feca3870b63a40aed89aaf209cac5cdcac4
cytsai0409 <cytsai0409@gmail.com>
2021-02-25 17:26:08 +0800

    [S9700-23D] Add platform S9700-23D
    S9700-23D (400G Disaggregated Core and Edge Router)
    - CPU: Intel Broadwell-DE
    - MAC: Broadcom BCM88690
    - Network: 10 x 400GE QSFP-DD service ports + 13 x 400GE QSFP-DD fabric ports
    
    Signed-off-by: Jason Tsai jason.cy.tsai@ufispace.com
    Signed-off-by: cytsai0409 <cytsai0409@gmail.com>

---
+++ packages/platforms/ufispace/x86-64/s9700-23d/onlp/builds/x86_64_ufispace_s9700_23d/module/auto/make.mk
@@ -0,0 +1,7 @@
+#
+# x86_64_ufispace_s9700_23d Autogeneration
+#
+###############################################################################
+x86_64_ufispace_s9700_23d_AUTO_DEFS := module/auto/x86_64_ufispace_s9700_23d.yml
+x86_64_ufispace_s9700_23d_AUTO_DIRS := module/inc/x86_64_ufispace_s9700_23d module/src
+include $(BUILDER)/auto.mk
