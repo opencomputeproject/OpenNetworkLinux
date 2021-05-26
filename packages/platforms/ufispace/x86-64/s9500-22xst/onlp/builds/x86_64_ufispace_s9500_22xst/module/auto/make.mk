c9e908a8709de8f83f274e9083df28bc2e0223f5
Alex Liu <alex.liu@ufispace.com>
2021-05-25 19:56:36 -0700

    [Ufispace][S9500-22xst] Add ufispace S9500-22xst support
    Signed-off-by: Alex Liu <alex.liu@ufispace.com>

---
+++ packages/platforms/ufispace/x86-64/s9500-22xst/onlp/builds/x86_64_ufispace_s9500_22xst/module/auto/make.mk
@@ -0,0 +1,7 @@
+#
+# x86_64_ufispace_s9500_22xst Autogeneration
+#
+###############################################################################
+x86_64_ufispace_s9500_22xst_AUTO_DEFS := module/auto/x86_64_ufispace_s9500_22xst.yml
+x86_64_ufispace_s9500_22xst_AUTO_DIRS := module/inc/x86_64_ufispace_s9500_22xst module/src
+include $(BUILDER)/auto.mk
