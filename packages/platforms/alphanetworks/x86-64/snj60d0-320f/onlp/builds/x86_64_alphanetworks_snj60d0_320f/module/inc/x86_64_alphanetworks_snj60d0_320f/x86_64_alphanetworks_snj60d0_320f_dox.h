4604db26ce8f942bad88fb51693257c1de436154
juntseng62 <juntseng62@gmail.com>
2021-07-14 16:35:42 +0800

    Add new platform SNJ60D0-320F
    Signed-off-by: juntseng62 <juntseng62@gmail.com>

---
+++ packages/platforms/alphanetworks/x86-64/snj60d0-320f/onlp/builds/x86_64_alphanetworks_snj60d0_320f/module/inc/x86_64_alphanetworks_snj60d0_320f/x86_64_alphanetworks_snj60d0_320f_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_alphanetworks_snj60d0_320f Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __X86_64_ALPHANETWORKS_SNJ60D0_320F_DOX_H__
+#define __X86_64_ALPHANETWORKS_SNJ60D0_320F_DOX_H__
+
+/**
+ * @defgroup x86_64_alphanetworks_snj60d0_320f x86_64_alphanetworks_snj60d0_320f - x86_64_alphanetworks_snj60d0_320f Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_alphanetworks_snj60d0_320f-x86_64_alphanetworks_snj60d0_320f Public Interface
+ * @defgroup x86_64_alphanetworks_snj60d0_320f-config Compile Time Configuration
+ * @defgroup x86_64_alphanetworks_snj60d0_320f-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __X86_64_ALPHANETWORKS_SNJ60D0_320F_DOX_H__ */
