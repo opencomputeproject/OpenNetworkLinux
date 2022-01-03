015160fced86d039c094424ba4cbd3576eddb204
juntseng62 <juntseng62@gmail.com>
2022-01-03 14:40:48 +0800

    Add new platform SNJ61D0-320F
    Signed-off-by: juntseng62 <juntseng62@gmail.com>

---
+++ packages/platforms/alphanetworks/x86-64/snj61d0-320f/onlp/builds/x86_64_alphanetworks_snj61d0_320f/module/src/x86_64_alphanetworks_snj61d0_320f_log.h
@@ -0,0 +1,12 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#ifndef __X86_64_ALPHANETWORKS_SNJ61D0_320F_LOG_H__
+#define __X86_64_ALPHANETWORKS_SNJ61D0_320F_LOG_H__
+
+#define AIM_LOG_MODULE_NAME x86_64_alphanetworks_snj61d0_320f
+#include <AIM/aim_log.h>
+
+#endif /* __X86_64_ALPHANETWORKS_SNJ61D0_320F_LOG_H__ */
