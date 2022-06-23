d4051c27c966bf96ec8edf663e50b5f3cb1a03c3
Jake Lin <jake_lin@edge-core.com>
2022-06-23 03:42:03 +0000

    [as7946_30xb] Add new platform
    CPU: Intel Broadwell-DE Xeon 1548, 8-core
    MAC: Broadcom BCM88823
    BMC: AST2600
    CPLD: Intel 5M1270ZF256C5N (2pcs, FBGA256 package)
    Ethernet Port: 4 x 400G QSFP-DD (also support high power 100G optics)
                   22 x 100G QSFP28
                   4 x 25G SFP28
    Power Supply: AC/DC or DC/DC max 1300W output, 1 + 1 redundant load-sharing
    Cooling: 5pcs hot swappable 6076, 12V fan, support 4 + 1 mode
    Dimension: 440mm (Width) x 480mm (Depth) x 87 mm (Height)
    
    Signed-off-by: Jake Lin <jake_lin@edge-core.com>

---
+++ packages/platforms/accton/x86-64/as7946-30xb/modules/builds/x86-64-accton-as7946-30xb-sys.c
@@ -0,0 +1,476 @@
+/*
+ * Copyright (C)  Jake Lin <jake_lin@edge-core.com>
+ *
+ * Based on:
+ *	pca954x.c from Kumar Gala <galak@kernel.crashing.org>
+ * Copyright (C) 2006
+ *
+ * Based on:
+ *	pca954x.c from Ken Harrenstien
+ * Copyright (C) 2004 Google, Inc. (Ken Harrenstien)
+ *
+ * Based on:
+ *	i2c-virtual_cb.c from Brian Kuschak <bkuschak@yahoo.com>
+ * 	and pca9540.c from Jean Delvare <khali@linux-fr.org>.
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2. This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/device.h>
+#include <linux/version.h>
+#include <linux/stat.h>
+#include <linux/sysfs.h>
+#include <linux/hwmon-sysfs.h>
+#include <linux/ipmi.h>
+#include <linux/ipmi_smi.h>
+#include <linux/platform_device.h>
+
+#define DRVNAME "as7946_30xb_sys"
+#define ACCTON_IPMI_NETFN       0x34
+
+#define IPMI_SYSEEPROM_READ_CMD 0x18
+#define IPMI_TIMEOUT		(20 * HZ)
+#define IPMI_READ_MAX_LEN       128
+
+#define EEPROM_NAME		"eeprom"
+#define EEPROM_SIZE		512	/*512 byte eeprom */
+
+#define IPMI_GET_CPLD_VER_CMD   0x20
+#define IPMI_GET_CPLD_CMD       0x22
+#define CPU_CPLD_ADDR           0x65
+#define SYSTEM_CPLD1_ADDR       0x60
+#define FAN_CPLD_ADDR           0x68
+
+static void ipmi_msg_handler(struct ipmi_recv_msg *msg, void *user_msg_data);
+static int as7946_30xb_sys_probe(struct platform_device *pdev);
+static int as7946_30xb_sys_remove(struct platform_device *pdev);
+static ssize_t show_cpld_version(struct device *dev, 
+	struct device_attribute *da, char *buf);
+
+struct ipmi_data {
+	struct completion   read_complete;
+	struct ipmi_addr    address;
+	struct ipmi_user *  user;
+	int                 interface;
+
+	struct kernel_ipmi_msg tx_message;
+	long                   tx_msgid;
+
+	void            *rx_msg_data;
+	unsigned short   rx_msg_len;
+	unsigned char    rx_result;
+	int              rx_recv_type;
+
+	struct ipmi_user_hndl ipmi_hndlrs;
+};
+
+struct as7946_30xb_sys_data {
+	struct platform_device *pdev;
+	struct mutex     update_lock;
+	char             valid;           /* != 0 if registers are valid */
+	unsigned long    last_updated;    /* In jiffies */
+	struct ipmi_data ipmi;
+	unsigned char    ipmi_resp_eeprom[EEPROM_SIZE];
+	unsigned char    ipmi_resp_cpld[2];
+	unsigned char    ipmi_tx_data[3];
+	struct bin_attribute eeprom;      /* eeprom data */
+};
+
+struct as7946_30xb_sys_data *data = NULL;
+
+static struct platform_driver as7946_30xb_sys_driver = {
+	.probe      = as7946_30xb_sys_probe,
+	.remove     = as7946_30xb_sys_remove,
+	.driver     = {
+		.name   = DRVNAME,
+		.owner  = THIS_MODULE,
+	},
+};
+
+enum as7946_30xb_sys_sysfs_attrs {
+	CPU_CPLD_VER, /* CPU board CPLD version */
+	SYSTEM_CPLD1_VER, /* System cpld1 version */
+	FAN_CPLD_VER, /* FAN CPLD version */
+};
+
+static SENSOR_DEVICE_ATTR(cpu_cpld_ver, S_IRUGO, show_cpld_version, 
+			  NULL, CPU_CPLD_VER);
+static SENSOR_DEVICE_ATTR(system_cpld1_ver, S_IRUGO, show_cpld_version, 
+			  NULL, SYSTEM_CPLD1_VER);
+static SENSOR_DEVICE_ATTR(fan_cpld_ver, S_IRUGO, show_cpld_version, 
+			  NULL, FAN_CPLD_VER);
+
+static struct attribute *as7946_30xb_sys_attributes[] = {
+	&sensor_dev_attr_cpu_cpld_ver.dev_attr.attr,
+	&sensor_dev_attr_system_cpld1_ver.dev_attr.attr,
+	&sensor_dev_attr_fan_cpld_ver.dev_attr.attr,
+	NULL
+};
+
+static const struct attribute_group as7946_30xb_sys_group = {
+	.attrs = as7946_30xb_sys_attributes,
+};
+
+/* Functions to talk to the IPMI layer */
+
+/* Initialize IPMI address, message buffers and user data */
+static int init_ipmi_data(struct ipmi_data *ipmi, int iface,
+			  struct device *dev)
+{
+	int err;
+
+	init_completion(&ipmi->read_complete);
+
+	/* Initialize IPMI address */
+	ipmi->address.addr_type = IPMI_SYSTEM_INTERFACE_ADDR_TYPE;
+	ipmi->address.channel = IPMI_BMC_CHANNEL;
+	ipmi->address.data[0] = 0;
+	ipmi->interface = iface;
+
+	/* Initialize message buffers */
+	ipmi->tx_msgid = 0;
+	ipmi->tx_message.netfn = ACCTON_IPMI_NETFN;
+
+	ipmi->ipmi_hndlrs.ipmi_recv_hndl = ipmi_msg_handler;
+
+	/* Create IPMI messaging interface user */
+	err = ipmi_create_user(ipmi->interface, &ipmi->ipmi_hndlrs,
+			       ipmi, &ipmi->user);
+	if (err < 0) {
+		dev_err(dev, "Unable to register user with IPMI "
+			"interface %d\n", ipmi->interface);
+		return -EACCES;
+	}
+
+	return 0;
+}
+
+/* Send an IPMI command */
+static int ipmi_send_message(struct ipmi_data *ipmi, unsigned char cmd,
+			     unsigned char *tx_data, unsigned short tx_len,
+			     unsigned char *rx_data, unsigned short rx_len)
+{
+	int err;
+
+	ipmi->tx_message.cmd      = cmd;
+	ipmi->tx_message.data     = tx_data;
+	ipmi->tx_message.data_len = tx_len;
+	ipmi->rx_msg_data         = rx_data;
+	ipmi->rx_msg_len          = rx_len;
+
+	err = ipmi_validate_addr(&ipmi->address, sizeof(ipmi->address));
+	if (err)
+		goto addr_err;
+
+	ipmi->tx_msgid++;
+	err = ipmi_request_settime(ipmi->user, &ipmi->address, ipmi->tx_msgid,
+				   &ipmi->tx_message, ipmi, 0, 0, 0);
+	if (err)
+		goto ipmi_req_err;
+
+	err = wait_for_completion_timeout(&ipmi->read_complete, IPMI_TIMEOUT);
+	if (!err)
+		goto ipmi_timeout_err;
+
+	return 0;
+
+ipmi_timeout_err:
+	err = -ETIMEDOUT;
+	dev_err(&data->pdev->dev, "request_timeout=%x\n", err);
+	return err;
+ipmi_req_err:
+	dev_err(&data->pdev->dev, "request_settime=%x\n", err);
+	return err;
+addr_err:
+	dev_err(&data->pdev->dev, "validate_addr=%x\n", err);
+	return err;
+}
+
+/* Dispatch IPMI messages to callers */
+static void ipmi_msg_handler(struct ipmi_recv_msg *msg, void *user_msg_data)
+{
+	unsigned short rx_len;
+	struct ipmi_data *ipmi = user_msg_data;
+
+	if (msg->msgid != ipmi->tx_msgid) {
+		dev_err(&data->pdev->dev, "Mismatch between received msgid "
+			"(%02x) and transmitted msgid (%02x)!\n",
+			(int)msg->msgid,
+			(int)ipmi->tx_msgid);
+		ipmi_free_recv_msg(msg);
+		return;
+	}
+
+	ipmi->rx_recv_type = msg->recv_type;
+	if (msg->msg.data_len > 0)
+		ipmi->rx_result = msg->msg.data[0];
+	else
+		ipmi->rx_result = IPMI_UNKNOWN_ERR_COMPLETION_CODE;
+
+	if (msg->msg.data_len > 1) {
+		rx_len = msg->msg.data_len - 1;
+		if (ipmi->rx_msg_len < rx_len)
+			rx_len = ipmi->rx_msg_len;
+		ipmi->rx_msg_len = rx_len;
+		memcpy(ipmi->rx_msg_data, msg->msg.data + 1, ipmi->rx_msg_len);
+	} else
+		ipmi->rx_msg_len = 0;
+
+	ipmi_free_recv_msg(msg);
+	complete(&ipmi->read_complete);
+}
+
+static ssize_t sys_eeprom_read(loff_t off, char *buf, size_t count)
+{
+	int status = 0;
+	unsigned char length = 0;
+
+	if ((off + count) > EEPROM_SIZE)
+		return -EINVAL;
+
+	length = (count >= IPMI_READ_MAX_LEN) ? IPMI_READ_MAX_LEN : count;
+	data->ipmi_tx_data[0] = off;
+	data->ipmi_tx_data[1] = length;
+	status = ipmi_send_message(&data->ipmi, IPMI_SYSEEPROM_READ_CMD, 
+					data->ipmi_tx_data, 2,
+					data->ipmi_resp_eeprom + off, length);
+	if (unlikely(status != 0))
+		goto exit;
+
+	if (unlikely(data->ipmi.rx_result != 0)) {
+		status = -EIO;
+		goto exit;
+	}
+
+	status = length; /* Read length */
+	memcpy(buf, data->ipmi_resp_eeprom + off, length);
+
+exit:
+    return status;
+}
+
+static ssize_t sysfs_bin_read(struct file *filp, struct kobject *kobj,
+		struct bin_attribute *attr,
+		char *buf, loff_t off, size_t count)
+{
+	ssize_t retval = 0;
+
+	if (unlikely(!count))
+		return count;
+
+	/*
+	 * Read data from chip, protecting against concurrent updates
+	 * from this host
+	 */
+	mutex_lock(&data->update_lock);
+
+	while (count) {
+		ssize_t status;
+
+		status = sys_eeprom_read(off, buf, count);
+		if (status <= 0) {
+			if (retval == 0)
+				retval = status;
+
+			break;
+		}
+
+		buf += status;
+		off += status;
+		count -= status;
+		retval += status;
+	}
+
+	mutex_unlock(&data->update_lock);
+	return retval;
+
+}
+
+static int sysfs_eeprom_init(struct kobject *kobj, 
+			     struct bin_attribute *eeprom)
+{
+	sysfs_bin_attr_init(eeprom);
+	eeprom->attr.name = EEPROM_NAME;
+	eeprom->attr.mode = S_IRUGO;
+	eeprom->read	  = sysfs_bin_read;
+	eeprom->write	  = NULL;
+	eeprom->size	  = EEPROM_SIZE;
+
+	/* Create eeprom file */
+	return sysfs_create_bin_file(kobj, eeprom);
+}
+
+static int sysfs_eeprom_cleanup(struct kobject *kobj, 
+				struct bin_attribute *eeprom)
+{
+	sysfs_remove_bin_file(kobj, eeprom);
+	return 0;
+}
+
+static struct as7946_30xb_sys_data *
+as7946_30xb_sys_update_cpld_ver(unsigned char cpld_addr)
+{
+	int status = 0;
+
+	data->valid = 0;
+	data->ipmi_tx_data[0] = cpld_addr;
+	status = ipmi_send_message(&data->ipmi, IPMI_GET_CPLD_VER_CMD, 
+				   data->ipmi_tx_data, 1,
+				   data->ipmi_resp_cpld, 
+				   sizeof(data->ipmi_resp_cpld));
+
+	if (unlikely(status != 0))
+		goto exit;
+
+	if (unlikely(data->ipmi.rx_result != 0)) {
+		status = -EIO;
+		goto exit;
+	}
+
+	data->last_updated = jiffies;
+	data->valid = 1;
+
+exit:
+	return data;
+}
+
+static ssize_t show_cpld_version(struct device *dev, 
+				 struct device_attribute *da, char *buf)
+{
+	struct sensor_device_attribute *attr = to_sensor_dev_attr(da);
+	unsigned char cpld_addr = 0;
+	unsigned int value = 0;
+	int error = 0;
+
+	switch (attr->index) {
+	case CPU_CPLD_VER:
+		cpld_addr = CPU_CPLD_ADDR;
+		break;
+	case SYSTEM_CPLD1_VER:
+		cpld_addr = SYSTEM_CPLD1_ADDR;
+		break;
+	case FAN_CPLD_VER:
+		cpld_addr = FAN_CPLD_ADDR;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	mutex_lock(&data->update_lock);
+
+	data = as7946_30xb_sys_update_cpld_ver(cpld_addr);
+	if (!data->valid) {
+		error = -EIO;
+		goto exit;
+	}
+	switch (attr->index) {
+	case CPU_CPLD_VER:
+		value = data->ipmi_resp_cpld[0];
+		break;
+	case SYSTEM_CPLD1_VER:
+		value = data->ipmi_resp_cpld[0] << 8 | data->ipmi_resp_cpld[1];
+		break;
+	case FAN_CPLD_VER:
+		value = data->ipmi_resp_cpld[0] << 8 | data->ipmi_resp_cpld[1];
+		break;
+	default:
+		return -EINVAL;
+	}
+	mutex_unlock(&data->update_lock);
+	return sprintf(buf, "%d\n", value);
+
+exit:
+	mutex_unlock(&data->update_lock);
+	return error;    
+}
+
+static int as7946_30xb_sys_probe(struct platform_device *pdev)
+{
+	int status = -1;
+
+	/* Register sysfs hooks */
+	status = sysfs_eeprom_init(&pdev->dev.kobj, &data->eeprom);
+	if (status)
+		goto exit;
+
+	/* Register sysfs hooks */
+	status = sysfs_create_group(&pdev->dev.kobj, &as7946_30xb_sys_group);
+	if (status) {
+		goto exit;
+	}
+
+	dev_info(&pdev->dev, "device created\n");
+
+	return 0;
+
+exit:
+	return status;
+}
+
+static int as7946_30xb_sys_remove(struct platform_device *pdev)
+{
+	sysfs_eeprom_cleanup(&pdev->dev.kobj, &data->eeprom);
+	sysfs_remove_group(&pdev->dev.kobj, &as7946_30xb_sys_group);
+
+	return 0;
+}
+
+static int __init as7946_30xb_sys_init(void)
+{
+	int ret;
+
+	data = kzalloc(sizeof(struct as7946_30xb_sys_data), GFP_KERNEL);
+	if (!data) {
+		ret = -ENOMEM;
+		goto alloc_err;
+	}
+
+	mutex_init(&data->update_lock);
+
+	ret = platform_driver_register(&as7946_30xb_sys_driver);
+	
+	if (ret < 0)
+		goto dri_reg_err;
+
+	data->pdev = platform_device_register_simple(DRVNAME, -1, NULL, 0);
+	if (IS_ERR(data->pdev)) {
+		ret = PTR_ERR(data->pdev);
+		goto dev_reg_err;
+	}
+
+	/* Set up IPMI interface */
+	ret = init_ipmi_data(&data->ipmi, 0, &data->pdev->dev);
+	if (ret)
+		goto ipmi_err;
+
+	return 0;
+
+ipmi_err:
+	platform_device_unregister(data->pdev);
+dev_reg_err:
+	platform_driver_unregister(&as7946_30xb_sys_driver);
+dri_reg_err:
+	kfree(data);
+alloc_err:
+	return ret;
+}
+
+static void __exit as7946_30xb_sys_exit(void)
+{
+	ipmi_destroy_user(data->ipmi.user);
+	platform_device_unregister(data->pdev);
+	platform_driver_unregister(&as7946_30xb_sys_driver);
+	kfree(data);
+}
+
+MODULE_AUTHOR("Jake Lin <jake_lin@edge-core.com>");
+MODULE_DESCRIPTION("AS7946-30XB System driver");
+MODULE_LICENSE("GPL");
+
+module_init(as7946_30xb_sys_init);
+module_exit(as7946_30xb_sys_exit);
---
+++ packages/platforms/accton/x86-64/as7946-30xb/onlp/builds/x86_64_accton_as7946_30xb/module/src/fani.c
@@ -0,0 +1,283 @@
+/************************************************************
+ * <bsn.cl fy=2014 v=onl>
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *           Copyright 2017 Accton Technology Corporation.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *
+ * </bsn.cl>
+ ************************************************************
+ *
+ * Fan Platform Implementation Defaults.
+ *
+ ***********************************************************/
+#include <onlp/platformi/fani.h>
+#include "platform_lib.h"
+
+enum fan_id {
+    FAN_1_ON_FAN_BOARD = 1,
+    FAN_2_ON_FAN_BOARD,
+    FAN_3_ON_FAN_BOARD,
+    FAN_4_ON_FAN_BOARD,
+    FAN_5_ON_FAN_BOARD,
+    FAN_1_ON_PSU_1,
+    FAN_1_ON_PSU_2,
+};
+
+#define MAX_PSU_FAN_SPEED 30000
+
+#define CHASSIS_FAN_INFO(fid)        \
+    { \
+        { ONLP_FAN_ID_CREATE(FAN_##fid##_ON_FAN_BOARD), "Chassis Fan - "#fid, 0, {0} },\
+        0x0,\
+        ONLP_FAN_CAPS_SET_PERCENTAGE | ONLP_FAN_CAPS_GET_RPM | ONLP_FAN_CAPS_GET_PERCENTAGE,\
+        0,\
+        0,\
+        ONLP_FAN_MODE_INVALID,\
+        { 0 },\
+        { 0 },\
+    }
+
+#define PSU_FAN_INFO(pid, fid) \
+    { \
+        { ONLP_FAN_ID_CREATE(FAN_##fid##_ON_PSU_##pid), "PSU "#pid" - Fan "#fid, 0, {0} },\
+        0x0,\
+        ONLP_FAN_CAPS_GET_RPM | ONLP_FAN_CAPS_GET_PERCENTAGE,\
+        0,\
+        0,\
+        ONLP_FAN_MODE_INVALID,\
+        { 0 },\
+        { 0 },\
+    }
+
+#define AIM_FREE_IF_PTR(p) \
+    do \
+    { \
+        if (p) { \
+            aim_free(p); \
+            p = NULL; \
+        } \
+    } while (0)
+
+/* Static fan information */
+onlp_fan_info_t finfo[] = {
+    { }, /* Not used */
+    CHASSIS_FAN_INFO(1),
+    CHASSIS_FAN_INFO(2),
+    CHASSIS_FAN_INFO(3),
+    CHASSIS_FAN_INFO(4),
+    CHASSIS_FAN_INFO(5),
+    PSU_FAN_INFO(1,1),
+    PSU_FAN_INFO(2,1),
+};
+
+#define VALIDATE(_id)                           \
+    do {                                        \
+        if(!ONLP_OID_IS_FAN(_id)) {             \
+            return ONLP_STATUS_E_INVALID;       \
+        }                                       \
+    } while(0)
+
+static int
+_onlp_fani_info_get_fan(int fid, onlp_fan_info_t* info)
+{
+    char *fandir = NULL;
+    int   len = 0;
+    int   value;
+
+    /* get fan present status
+     */
+    if (onlp_file_read_int(&value, "%s""fan%d_present", FAN_BOARD_PATH, fid) < 0) {
+        AIM_LOG_ERROR("Unable to read status from (%s)\r\n", FAN_BOARD_PATH);
+        return ONLP_STATUS_E_INTERNAL;
+    }
+
+    if (value == 0) {
+        return ONLP_STATUS_OK; /* fan is not present */
+    }
+    info->status |= ONLP_FAN_STATUS_PRESENT;
+
+    /* get fan direction
+     */
+    len = onlp_file_read_str(&fandir, "%s""fan%d_dir", FAN_BOARD_PATH, fid);
+    if (fandir && len) {
+        if (strncmp(fandir, "B2F", strlen("B2F")) == 0) {
+            info->status |= ONLP_FAN_STATUS_B2F;
+        }
+        else {
+            info->status |= ONLP_FAN_STATUS_F2B;
+        }
+    }
+    AIM_FREE_IF_PTR(fandir);
+
+    /* get front fan speed
+     */
+    if (onlp_file_read_int(&value, "%s""fan%d_input", FAN_BOARD_PATH, fid) < 0) {
+        AIM_LOG_ERROR("Unable to read status from (%s)\r\n", FAN_BOARD_PATH);
+        return ONLP_STATUS_E_INTERNAL;
+    }
+    info->rpm = value;
+
+    /* get rear fan speed
+     */
+    if (onlp_file_read_int(&value, "%s""fan%d_input", FAN_BOARD_PATH, fid+CHASSIS_FAN_COUNT) < 0) {
+        AIM_LOG_ERROR("Unable to read status from (%s)\r\n", FAN_BOARD_PATH);
+        return ONLP_STATUS_E_INTERNAL;
+    }
+
+    /* get front fan percentage
+     */
+    if (onlp_file_read_int(&value, "%s""fan%d_pwm", FAN_BOARD_PATH, fid) < 0) {
+        AIM_LOG_ERROR("Unable to read status from (%s)\r\n", FAN_BOARD_PATH);
+        return ONLP_STATUS_E_INTERNAL;
+    }
+    info->percentage = value;
+
+    /* get rear fan percentage
+     */
+    if (onlp_file_read_int(&value, "%s""fan%d_pwm", FAN_BOARD_PATH, fid+CHASSIS_FAN_COUNT) < 0) {
+        AIM_LOG_ERROR("Unable to read status from (%s)\r\n", FAN_BOARD_PATH);
+        return ONLP_STATUS_E_INTERNAL;
+    }
+    /* take the min value from front/rear fan speed
+     */
+    if (info->percentage > value) {
+        info->percentage = value;
+    }
+
+    /* get fan fault status (turn on when any one fails)
+     */
+    if (info->rpm == 0) {
+        info->status |= ONLP_FAN_STATUS_FAILED;
+    }
+
+    return ONLP_STATUS_OK;
+}
+
+static int
+_onlp_fani_info_get_fan_on_psu(int pid, onlp_fan_info_t* info)
+{
+    char *fandir = NULL;
+    int   len = 0;
+    int   val = 0;
+    int   ret = 0;
+
+    info->status |= ONLP_FAN_STATUS_PRESENT;
+
+    /* get fan direction
+     */
+    len = onlp_file_read_str(&fandir, "%s""psu%d_fan_dir", PSU_SYSFS_PATH, pid);
+    if (fandir && len) {
+        if (strncmp(fandir, "B2F", strlen("B2F")) == 0) {
+            info->status |= ONLP_FAN_STATUS_B2F;
+        }
+        else {
+            info->status |= ONLP_FAN_STATUS_F2B;
+        }
+    }
+    AIM_FREE_IF_PTR(fandir);
+
+    /* get fan speed
+     */
+    ret = onlp_file_read_int(&val, "%s""psu%d_fan1_input", PSU_SYSFS_PATH, pid);
+    if (ret < 0) {
+        AIM_LOG_ERROR("Unable to read status from (%s""psu%d_fan1_input)\r\n", PSU_SYSFS_PATH, pid);
+        return ONLP_STATUS_E_INTERNAL;
+    }
+
+    /* get speed percentage from rpm
+     */
+    info->rpm = val;
+    info->percentage = (info->rpm * 100)/MAX_PSU_FAN_SPEED;
+
+    return ONLP_STATUS_OK;
+}
+
+/*
+ * This function sets the fan speed of the given OID as a percentage.
+ *
+ * This will only be called if the OID has the PERCENTAGE_SET
+ * capability.
+ *
+ * It is optional if you have no fans at all with this feature.
+ */
+int
+onlp_fani_percentage_set(onlp_oid_t id, int p)
+{
+    int fid;
+
+    VALIDATE(id);
+
+    fid = ONLP_OID_ID_GET(id);
+
+    /* reject p=0 (p=0, stop fan) */
+    if (p == 0){
+        AIM_LOG_ERROR("Zero percentage is not allowed\r\n");
+        return ONLP_STATUS_E_INVALID;
+    }
+
+    if (fid < FAN_1_ON_FAN_BOARD || fid > FAN_5_ON_FAN_BOARD) {
+        AIM_LOG_ERROR("Invalid fan id(%d)\r\n", fid);
+        return ONLP_STATUS_E_INVALID;
+    }
+
+    if (onlp_file_write_int(p, "%s%s%d%s", FAN_BOARD_PATH, "fan", fid, "_pwm") < 0) {
+        AIM_LOG_ERROR("Unable to write data to file (%s%s)\r\n", FAN_BOARD_PATH, "fan_pwm");
+        return ONLP_STATUS_E_INTERNAL;
+    }
+
+    return ONLP_STATUS_OK;
+}
+
+
+/*
+ * This function will be called prior to all of onlp_fani_* functions.
+ */
+int
+onlp_fani_init(void)
+{
+    return ONLP_STATUS_OK;
+}
+
+int
+onlp_fani_info_get(onlp_oid_t id, onlp_fan_info_t* info)
+{
+    int rc = 0;
+    int fid;
+    VALIDATE(id);
+
+    fid = ONLP_OID_ID_GET(id);
+    *info = finfo[fid];
+
+    switch (fid) {
+    case FAN_1_ON_PSU_1:
+    case FAN_1_ON_PSU_2:
+        rc = _onlp_fani_info_get_fan_on_psu(fid-FAN_5_ON_FAN_BOARD, info);
+        break;
+    case FAN_1_ON_FAN_BOARD:
+    case FAN_2_ON_FAN_BOARD:
+    case FAN_3_ON_FAN_BOARD:
+    case FAN_4_ON_FAN_BOARD:
+    case FAN_5_ON_FAN_BOARD:
+        rc =_onlp_fani_info_get_fan(fid, info);
+        break;
+    default:
+        AIM_LOG_ERROR("Invalid fan id(%d)\r\n", fid);
+        rc = ONLP_STATUS_E_INVALID;
+        break;
+    }
+
+    return rc;
+}
