4897d6f9debc6d2ed80146bc38c5d752c71c2ac6
Brandon Chuang <brandon_chuang@edge-core.com>
2022-07-18 16:29:31 +0800

    [Edgecore][as7535_28xb] Add BMC detection in __init__.py
    Detect BMC in __init__.py to make sure /dev/ipmi insterface is ready
    
    Signed-off-by: Brandon Chuang <brandon_chuang@edge-core.com>

--- packages/platforms/accton/x86-64/as7535-28xb/platform-config/r0/src/python/x86_64_accton_as7535_28xb_r0/__init__.py
+++ packages/platforms/accton/x86-64/as7535-28xb/platform-config/r0/src/python/x86_64_accton_as7535_28xb_r0/__init__.py
@@ -1,5 +1,69 @@
+import commands
+from itertools import chain
 from onl.platform.base import *
 from onl.platform.accton import *
+from time import sleep
+
+init_ipmi_dev = [
+    'echo "remove,kcs,i/o,0xca2" > /sys/module/ipmi_si/parameters/hotmod',
+    'echo "add,kcs,i/o,0xca2" > /sys/module/ipmi_si/parameters/hotmod']
+
+ATTEMPTS = 5
+INTERVAL = 3
+
+def init_ipmi_dev_intf():
+    attempts = ATTEMPTS
+    interval = INTERVAL
+
+    while attempts:
+        if os.path.exists('/dev/ipmi0') or os.path.exists('/dev/ipmidev/0'):
+            return (True, (ATTEMPTS - attempts) * interval)
+
+        for i in range(0, len(init_ipmi_dev)):
+            commands.getstatusoutput(init_ipmi_dev[i])
+
+        attempts -= 1
+        sleep(interval)
+
+    return (False, ATTEMPTS * interval)
+
+def init_ipmi_oem_cmd():
+    attempts = ATTEMPTS
+    interval = INTERVAL
+
+    while attempts:
+        status, output = commands.getstatusoutput('ipmitool raw 0x34 0x95')
+        if status:
+            attempts -= 1
+            sleep(interval)
+            continue
+
+        return (True, (ATTEMPTS - attempts) * interval)
+
+    return (False, ATTEMPTS * interval)
+
+def init_ipmi():
+    attempts = ATTEMPTS
+    interval = 60
+
+    while attempts:
+        attempts -= 1
+
+        (status, elapsed_dev) = init_ipmi_dev_intf()
+        if status is not True:
+            sleep(interval - elapsed_dev)
+            continue
+
+        (status, elapsed_oem) = init_ipmi_oem_cmd()
+        if status is not True:
+            sleep(interval - elapsed_dev - elapsed_oem)
+            continue
+
+        print('IPMI dev interface is ready.')
+        return True
+
+    print('Failed to initialize IPMI dev interface')
+    return False
 
 class OnlPlatform_x86_64_accton_as7535_28xb_r0(OnlPlatformAccton,
                                               OnlPlatformPortConfig_2x100_2x400_24x25):
@@ -8,6 +72,9 @@ class OnlPlatform_x86_64_accton_as7535_28xb_r0(OnlPlatformAccton,
     SYS_OBJECT_ID=".7535.28"
 
     def baseconfig(self):
+        if init_ipmi() is not True:
+            return False
+
         self.insmod('optoe')
         for m in [ 'sys', 'cpld', 'fan', 'psu', 'thermal', 'leds' ]:
             self.insmod("x86-64-accton-as7535-28xb-%s" % m)
