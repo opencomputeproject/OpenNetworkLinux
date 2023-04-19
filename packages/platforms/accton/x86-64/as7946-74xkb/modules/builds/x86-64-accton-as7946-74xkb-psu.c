de762deb5f002f13d4e700d76df7f221be26b638
brandonchuang <brandon_chuang@edge-core.com>
2023-04-19 14:59:46 +0800

    [Edgecore][as7946_74xkb] Update PSU/FAN index for OpenBMC v00.01.04
    Signed-off-by: brandonchuang <brandon_chuang@edge-core.com>

--- packages/platforms/accton/x86-64/as7946-74xkb/modules/builds/x86-64-accton-as7946-74xkb-psu.c
+++ packages/platforms/accton/x86-64/as7946-74xkb/modules/builds/x86-64-accton-as7946-74xkb-psu.c
@@ -397,7 +397,7 @@ static struct as7946_74xkb_psu_data *as7946_74xkb_psu_update_device(struct devic
 	data->ipmi_resp[pid].status[PSU_VOUT_MODE] = 0xff;
 
 	/* Get status from ipmi */
-	data->ipmi_tx_data[0] = pid + 1; /* PSU ID base id for ipmi start from 1 */
+	data->ipmi_tx_data[0] = pid; /* PSU ID base id for ipmi start from 0 */
 	status = ipmi_send_message(&data->ipmi, IPMI_PSU_READ_CMD,
 								data->ipmi_tx_data, 1,
 								data->ipmi_resp[pid].status,
