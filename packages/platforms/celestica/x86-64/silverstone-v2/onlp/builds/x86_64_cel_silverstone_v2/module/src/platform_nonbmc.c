1b10065ec23c823793d8b414b08ba158155b13e4
ingridcelestica <inhuang@celestica.com>
2023-10-13 06:19:24 +0000

    Fix redefinition issues
    Signed-off-by: ingridcelestica <inhuang@celestica.com>

--- packages/platforms/celestica/x86-64/silverstone-v2/onlp/builds/x86_64_cel_silverstone_v2/module/src/platform_nonbmc.c
+++ packages/platforms/celestica/x86-64/silverstone-v2/onlp/builds/x86_64_cel_silverstone_v2/module/src/platform_nonbmc.c
@@ -18,7 +18,17 @@
 #include <time.h>
 #include <sys/stat.h>
 #include <linux/i2c-dev.h>
+
+/*
+ * linux/i2c-dev.h provided by i2c-tools contains the symbols defined in linux/i2c.h.
+ * This is not usual, but some distros like OpenSuSe does it. The i2c.h will be only
+ * included if a well-known symbol is not defined, it avoids to redefine symbols and
+ * breaks the build.(refer issue#96)
+ */
+#ifndef I2C_FUNC_I2C
 #include <linux/i2c.h>
+#endif
+
 #include <onlplib/i2c.h>
 #include "platform_nonbmc.h"
 #include "platform_common.h"
